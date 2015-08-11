5a1a8d59a96d445b2269a3f32afaf4e5a2efba08
Martin FV <martinv@foliovision.com>
2015-08-11 10:34:11 +0200

    Fix for upgrade issues, part 2.

--- controller/backend.php
+++ controller/backend.php
@@ -257,6 +257,15 @@ function fv_wp_flowplayer_admin_notice() {
           </div>';
   }
   
+  global $FV_Player_Pro;
+  if( $FV_Player_Pro && version_compare($FV_Player_Pro->version,'0.5') != 1 ) : 
+  ?>
+  <div class="error">
+      <p><?php _e( 'FV Wordpress Flowplayer: Your pro extension is intalled, but it\'s not compatible with FV Flowplayer 6! Make sure you upgrade your FV Player Pro to version 0.5 or above.', 'my-text-domain' ); ?></p>
+  </div>
+  <?php
+  endif;
+  
   /*if( isset($_GET['page']) && $_GET['page'] == 'backend.php' ) {
 	  $options = get_option( 'fvwpflowplayer' );
     if( $options['key'] == 'false' ) {
