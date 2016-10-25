568222919ad7e261e655294b4e92810f6d330fb9
Martin FV <martinv@foliovision.com>
2016-10-25 14:50:31 +0200

    Minor settings screen glitch fix.

--- controller/backend.php
+++ controller/backend.php
@@ -558,7 +558,7 @@ function fv_wp_flowplayer_after_plugin_row( $arg) {
 <tr class="plugin-update-tr fv-wordpress-flowplayer-tr">
 	<td class="plugin-update colspanchange" colspan="3">
 		<div class="update-message">
-			<a href="http://foliovision.com/wordpress/plugins/fv-wordpress-flowplayer/download">All Licenses 20% Off</a> - Christmas sale!
+			<a href="http://foliovision.com/wordpress/plugins/fv-wordpress-flowplayer/download">All Licenses 20% Off</a> - Halloween sale!
 		</div>
 	</td>
 </tr>
