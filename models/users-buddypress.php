de781c85a74f55ae334587a63f9bfbae6e70e3f1
TheYurry <kubrican.juraj@gmail.com>
2016-12-23 15:35:15 +0100

    nicer code

--- models/users-buddypress.php
+++ models/users-buddypress.php
@@ -18,6 +18,7 @@ class FV_Player_BuddyPress {
   
   function add_video_tabs() {
     global $bp; 
+    
     bp_core_new_nav_item( array(
       'name'                  => 'Videos',
       'slug'                  => 'videos',
@@ -29,41 +30,30 @@ class FV_Player_BuddyPress {
     ) );
   }
 
-
   function videos_screen() {
 
     add_action( 'bp_template_title', array( $this, 'videos_screen_title') );
     add_action( 'bp_template_content', array( $this, 'profile') );
     bp_core_load_template( apply_filters( 'bp_core_template_plugin', 'members/single/plugins' ) );
   }
+  
   function videos_screen_title() { 
     global $bp;
     echo $bp->displayed_user->fullname . '\'s Videos<br/>';
   }
-  function videos_screen_content() { 
-    echo 'Animals Content<br/>';
-  }
 
-    function profile( $content ) {
-      global $bp;
-      //var_dump($bp->displayed_user);
-      $user_id = $bp->displayed_user->id;
+  function profile( $content ) {
+    global $bp;
+    $user_id = $bp->displayed_user->id;
 
-      $objVideos = new FV_Player_Custom_Videos( array( 'id' => $user_id, 'type' => 'user' ) );
+    $objVideos = new FV_Player_Custom_Videos( array( 'id' => $user_id, 'type' => 'user' ) );
 
-      if( get_current_user_id() === $user_id){
-        echo $objVideos->get_form();
-      }else{
-        echo $objVideos->get_html();
-      }
-      
-      
-      
-      
+    if( get_current_user_id() === $user_id){
+      echo $objVideos->get_form();
+    }else{
+      echo $objVideos->get_html();
     }
-
-
   }
-
+}
 
 $FV_Player_BuddyPress = new FV_Player_BuddyPress();
