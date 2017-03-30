c32dddb82ae8751e1ce42cbf7189cf510c163bc3
Martin FV <martinv@foliovision.com>
2017-03-30 17:26:23 +0200

    SEO - each player item should be a separate schema.org item.

--- models/seo.php
+++ models/seo.php
@@ -7,22 +7,9 @@ class FV_Player_SEO {
   public function __construct() {
     add_filter('fv_flowplayer_args_pre', array($this, 'should_i'), 10, 3 );
     add_filter('fv_flowplayer_attributes', array($this, 'single_attributes'), 10, 3 );
-    add_filter('fv_player_playlist_attributes', array($this, 'playlist_attributes'), 10, 3 );
     add_filter('fv_flowplayer_inner_html', array($this, 'single_video_seo'), 10, 2 );
     add_filter('fv_player_item_html', array($this, 'playlist_video_seo'), 10, 6 );
 
-  }
-  
-  function playlist_attributes( $attributes, $media, $fv_fp ) {
-    if( !$fv_fp->aCurArgs['playlist'] || !$this->can_seo ) {
-      return $attributes;
-    }
-    
-    $attributes['itemprop'] = 'video';
-    $attributes['itemscope'] = '';
-    $attributes['itemtype'] = 'http://schema.org/VideoObject';
-    
-    return $attributes;
   }
   
   function single_attributes( $attributes, $media, $fv_fp ) {
@@ -72,6 +59,7 @@ class FV_Player_SEO {
   
   function playlist_video_seo( $sHTML, $aArgs, $sSplashImage, $sItemCaption, $aPlayer, $index ) {
     if( $this->can_seo ) {
+      $sHTML = str_replace( '<a', '<a itemprop="video" itemscope itemtype="http://schema.org/VideoObject" ', $sHTML );
       $sHTML = str_replace( '</a>', $this->get_markup($sItemCaption,false,$sSplashImage,false).'</a>', $sHTML );
     }
     return $sHTML;
