98ff037cdaae75fa8ed011f9b7b4b63001208197
Martin Ambrus <martin@martinambrus.com>
2017-12-06 11:34:58 +0100

    refactor: admin JS needs to react to all current skin style changes

--- js/admin.js
+++ js/admin.js
@@ -12,54 +12,137 @@
       parseInt(result[3], 16)
     ] : null;
   }
-  
+
+  if (!String.prototype.endsWith)
+    String.prototype.endsWith = function(searchStr, Position) {
+      // This works much better than >= because
+      // it compensates for NaN:
+      if (!(Position < this.length))
+        Position = this.length;
+      else
+        Position |= 0; // round position
+      return this.substr(Position - searchStr.length,
+        searchStr.length) === searchStr;
+    };
+
   $(document).ready(function () {
-    $('[data-fv-preview]').on('input change', function () {
-      
-      var style = '';
-      
-      $('[data-fv-preview]').each(function () {
-        
-        if ($(this).attr('name') === 'player-position') {
-          if ($(this).val() === 'left')
-            style += $(this).data('fv-preview');
-        
-        } else if ($(this).attr('name') === 'subtitleBgColor') {
-          var replacement = hexToRgb($(this).val());
-          replacement.push($('#subtitleBgAlpha').val());
-          style += $(this).data('fv-preview').replace(/%val%/g, replacement.join(', '));
-        }else if($(this).attr('name')=== 'hasBorder'){
-          if($(this).prop('checked')){
-            style += $(this).data('fv-preview').replace(/%val%/g, '1');
-          }else{
-            style += $(this).data('fv-preview').replace(/%val%/g, '0');
+    $('[data-fv-skin]').on('input click', function () {
+
+      // hide currently visible settings tables
+      $('#skin-Custom-settings, #skin-Slim-settings, #skin-YouTubey-settings').hide();
+
+      // show the relevant settings table
+      $('#' + this.id + '-settings').show();
+
+      // update CSS
+      skinPreviewInputChanged();
+    });
+
+    // cache this, it's quite expensive to select via data attribute
+    var $previewElements = $('[data-fv-preview]');
+
+    // dropdown value changes (slim type, icon types)
+    function skinPreviewDropdownChanged() {
+      $previewElements.each(function() {
+        var
+          $this = $(this),
+          $parent = $this.closest('table')
+
+        // don't change to values of another skin but to our currently visible skin type
+        if ($parent.css('display') == 'none') {
+          return;
+        }
+
+        // playlist design style change
+        if ($this.attr('name').endsWith('playlist-design')) {
+          var
+            $external_playlist = $('.fp-playlist-external'),
+            match = $external_playlist.attr('class').match(/fv-playlist-design-\S+/);
+
+          if (match) {
+            $external_playlist.removeClass(match[0]);
           }
-            
+
+          $external_playlist
+            .removeClass('visible-captions')
+            .addClass('fv-playlist-design-' + $this.val());
+
+        } else if ($this.attr('name').endsWith('design-timeline')) {
+          // timeline design style change
+          $('.flowplayer')
+            .removeClass('fp-slim fp-full fp-fat fp-minimal')
+            .addClass($this.val());
+        } else if ($this.attr('name').endsWith('design-icons')) {
+          $('.flowplayer')
+            .removeClass('fp-edgy fp-outlined fp-playful')
+            .addClass($this.val());
+        }
+      });
+    }
+
+    // input (textbox, checkbox) value changes
+    function skinPreviewInputChanged() {
+      var style = '';
+
+      $previewElements.each(function () {
+
+        var
+          $this = $(this),
+          $parent = $this.closest('table')
+
+        if ($parent.css('display') == 'none') {
+          return;
+        }
+
+        if ($this.attr('name').endsWith('player-position')) {
+          if ($this.val() === 'left')
+            style += $this.data('fv-preview');
+
+        } else if ($this.attr('name').endsWith('subtitleBgColor')) {
+          var replacement = hexToRgb($this.val());
+          replacement.push($('#subtitleBgAlpha').val());
+          var newStyle = $this.data('fv-preview').replace(/%val%/g, replacement.join(', '));
+
+          if (newStyle.indexOf('#transparent') > -1) {
+            newStyle = newStyle.replace(/#transparent/g, 'transparent');
+          }
+
+          style += newStyle;
+        } else if($this.attr('name').endsWith('hasBorder')) {
+          var newStyle = '';
+
+          if ($this.prop('checked')) {
+            newStyle = $this.data('fv-preview').replace(/%val%/g, '1');
+          } else {
+            newStyle = $this.data('fv-preview').replace(/%val%/g, '0');
+          }
+
+          if (newStyle.indexOf('#transparent') > -1) {
+            newStyle = newStyle.replace(/#transparent/g, 'transparent');
+          }
+
+          style += newStyle;
         } else {
-          style += $(this).data('fv-preview').replace(/%val%/g, $(this).val());
+          var newStyle = $this.data('fv-preview').replace(/%val%/g, $this.val());;
+
+          if (newStyle.indexOf('#transparent') > -1) {
+            newStyle = newStyle.replace(/#transparent/g, 'transparent');
+          }
+
+          style += newStyle;
         }
       }, 0);
       $('#fv-style-preview').html(style);
-    }).trigger('input');
-    
-    $('#playlist-design').on('change', function () {
-      var match = jQuery('.fp-playlist-external').attr('class').match(/fv-playlist-design-\S+/)
-      if( match ) {
-        jQuery('.fp-playlist-external').removeClass(match[0]);
-      }
-      jQuery('.fp-playlist-external').removeClass('visible-captions');
-      jQuery('.fp-playlist-external').addClass('fv-playlist-design-'+$(this).val());
-    });
-    
-    $('#design-timeline').on('change', function () {
-      jQuery('.flowplayer').removeClass('fp-slim fp-full fp-fat fp-minimal');
-      jQuery('.flowplayer').addClass( $(this).val() );
-    });
-    
-    $('#design-icons').on('change', function () {
-      jQuery('.flowplayer').removeClass('fp-edgy fp-outlined fp-playful');
-      jQuery('.flowplayer').addClass( $(this).val() );
-    });        
+
+      // update progressbar & icons styles
+      skinPreviewDropdownChanged();
+    }
+
+    // color inputs + checkbox changes
+    $previewElements.on('input change', skinPreviewInputChanged).trigger('input');
+
+    $('[data-fv-preview]').on('select change', skinPreviewDropdownChanged);
+
   });
 
   
