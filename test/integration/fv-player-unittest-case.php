99a511d9d1d8737444cdd8c6fe5eac905ba08121
fvmartin <fvmartin@users.noreply.github.com>
2018-02-28 14:06:17 +0100

    Dividing integration tests into backend and frontend.

---
+++ test/integration/fv-player-unittest-case.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
+  
+  protected $backupGlobals = false;
+  
+  public function fix_newlines( $html ) {
+    $html = preg_replace( '/(id|rel)="wpfp_[^"]+"/', '$1="some-test-hash"', $html);
+    $html = preg_replace( '~<input type="hidden" id="([^"]*?)nonce" name="([^"]*?)nonce" value="([^"]*?)" />~', '<input type="hidden" id="$1nonce" name="$2nonce" value="XYZ" />', $html);
+    $html = preg_replace( "~nonce: '([^']*?)'~", "nonce: 'XYZ'", $html);
+    $html = explode("\n",$html);
+    $html = implode( "\n", array_map('trim',$html) );
+    return $html;
+  }
+
+  // we need to set up PRO player with an appropriate key, or the PRO player won't work
+  public static function wpSetUpBeforeClass() {
+    global $fv_fp;
+
+    // without this included, fv_wp_flowplayer_delete_extensions_transients() would not be found
+    //include_once "../../../fv-wordpress-flowplayer/controller/backend.php";
+
+    // include the flowplayer loader
+    include_once "../../../fv-wordpress-flowplayer/flowplayer.php";
+
+    // include the PRO plugin class, so it can intercept data saving
+    // and update the ads structure as needed for saving
+    //include_once "../../beta/fv-player-pro.class.php";
+
+    // save initial settings
+    //$fv_fp->_set_conf();
+  }
+
+}
