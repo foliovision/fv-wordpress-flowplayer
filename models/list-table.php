b6fd9f89540da728d83293074d67a5f07a77c3d8
Martin Vicenik <martinv@MartinCastle.local>
2018-07-16 15:50:43 +0200

    Adding search box and some styling.

--- models/list-table.php
+++ models/list-table.php
@@ -8,10 +8,9 @@ if ( ! class_exists( 'WP_List_Table' ) ) {
 
 class FV_Player_List_Table_View {
 
-  private $slug;
-
   function __construct() {
     add_action( 'admin_menu', array($this, 'admin_menu') );
+    add_action( 'admin_head', array($this, 'styling') );
   }
 
   function admin_menu(){    
@@ -21,32 +20,34 @@ class FV_Player_List_Table_View {
     }
   }
   
+  function styling() {
+    if( isset($_GET['page']) && $_GET['page'] == 'fv_player' ) {
+      global $fv_wp_flowplayer_ver;
+      wp_enqueue_style('fv-player-list-view', flowplayer::get_plugin_url().'/css/list-view.css',array(), $fv_wp_flowplayer_ver );
+    }
+  }
+  
   function tools_panel() {
 		$table = new FV_Player_List_Table();
 		$table->prepare_items();
   	?>
   	<div class="wrap">
-      <div>
-          <div id="icon-options-general" class="icon32"><br /></div>
-          <h2>FV Player</h2>
+      <h1 class="wp-heading-inline">FV Player</h1>
+      <a href="#" class="page-title-action">Add New</a>  
+      <a href="#" class="page-title-action fv-player-import">Import</a>      
+
+      <div id="fv_player_players_table">
+          <form id="fv-player-filter" method="get" action="<?php echo admin_url( 'admin.php?page=fv_player' ); ?>">
+              <input type="hidden" name="page" value="fv_player" />
+
+              <?php $table->views() ?>
+
+              <?php $table->advanced_filters(); ?>
+
+              <?php $table->display() ?>
+          </form>
       </div>
-
-        <p>
-            <a href="#" class="fv-player-import">IMPORT PLAYERS</a>
-        </p>
-
-        <div id="fv_player_players_table">
-            <form id="<?php echo $this->slug; ?>-filter" method="get" action="<?php echo admin_url( 'admin.php?page='.$this->slug ); ?>">
-                <input type="hidden" name="page" value="<?php echo $this->slug; ?>" />
-
-                <?php $table->views() ?>
-
-                <?php $table->advanced_filters(); ?>
-
-                <?php $table->display() ?>
-            </form>
-        </div>
-      
+    
   	</div>
   <?php 
     fv_player_shortcode_editor_scripts_enqueue();
@@ -90,77 +91,25 @@ class FV_Player_List_Table extends WP_List_Table {
 	}
 	
 	public function advanced_filters() {
-		$start_date = isset( $_GET['start-date'] )  ? sanitize_text_field( $_GET['start-date'] ) : null;
-		$end_date   = isset( $_GET['end-date'] )    ? sanitize_text_field( $_GET['end-date'] )   : null;
-		?>
-		<style>
-		#fv-tr-employees-timelog-filter {
-			background: #f5f5f5;
-    	background-image: none;
-			clear: both;
-			background-image: -webkit-gradient(linear,left bottom,left top,from(#f5f5f5),to(#fafafa));
-			background-image: -webkit-linear-gradient(bottom,#f5f5f5,#fafafa);
-			background-image: -moz-linear-gradient(bottom,#f5f5f5,#fafafa);
-			background-image: -o-linear-gradient(bottom,#f5f5f5,#fafafa);
-			background-image: linear-gradient(to top,#f5f5f5,#fafafa);
-			border-color: #dfdfdf;
-			border-width: 1px;
-			border-style: solid;
-			border-radius: 3px;
-			font-size: 13px;
-			line-height: 2.1em;
-			overflow: auto;
-			padding: 12px;
-			margin: 8px 0;
-		}
-		
-		.fv-tweet-value-edit { display: none }
-		.column-impressions, .column-clicks, .column-user_id, .column-ctr { width: 10% }
-		.wp-list-table input { width: 100% }
-		.column-edit { width: 3em }
-    .widefat .column-text { color: #888 }
-		</style>
-		<div id="fv-tr-employees-timelog-filter">
-			<span id="edd-payment-after-core-filters">
-				<input type="submit" class="button-secondary" value="<?php _e( 'Apply', 'easy-digital-downloads' ); ?>"/>
-			</span>
-			<?php if( ! empty( $status ) ) : ?>
-				<input type="hidden" name="status" value="<?php echo esc_attr( $status ); ?>"/>
-			<?php endif; ?>
-			<?php if( ! empty( $start_date ) || ! empty( $end_date ) ) : ?>
-				<a href="<?php echo admin_url( 'admin.php?page=fv_player' ); ?>" class="button-secondary"><?php _e( 'Clear Filter', 'easy-digital-downloads' ); ?></a>
-			<?php endif; ?>
-			<?php //$this->search_box( __( 'Search', 'easy-digital-downloads' ), 'edd-payments' ); ?>
-		</div>
-
-<?php
-	}	
-	
-	public function search_box( $text, $input_id ) {
-		if ( empty( $_REQUEST['s'] ) && !$this->has_items() )
-			return;
-
-		$input_id = $input_id . '-search-input';
-
-		if ( ! empty( $_REQUEST['orderby'] ) )
-			echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
-		if ( ! empty( $_REQUEST['order'] ) )
-			echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
-?>
-		<p class="search-box">
-			<?php do_action( 'edd_payment_history_search' ); ?>
-			<label class="screen-reader-text" for="<?php echo $input_id ?>"><?php echo $text; ?>:</label>
-			<input type="search" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>" />
-			<?php submit_button( $text, 'button', false, false, array('ID' => 'search-submit') ); ?><br/>
-		</p>
-<?php
+    if ( ! empty( $_REQUEST['orderby'] ) )
+      echo '<input type="hidden" name="orderby" value="' . esc_attr( $_REQUEST['orderby'] ) . '" />';
+    if ( ! empty( $_REQUEST['order'] ) )
+      echo '<input type="hidden" name="order" value="' . esc_attr( $_REQUEST['order'] ) . '" />';
+    ?>
+    <p class="search-box">
+      <?php do_action( 'edd_payment_history_search' ); ?>
+      <label class="screen-reader-text" for="<?php echo $input_id ?>">Search players:</label>
+      <input type="search" id="<?php echo $input_id ?>" name="s" value="<?php _admin_search_query(); ?>" />
+      <?php submit_button( "Search players", 'button', false, false, array('ID' => 'search-submit') ); ?><br/>
+    </p>
+    <?php
 	}
   
 	public function get_columns() {
 		return array(
 			//'cb'          => '<input type="checkbox" />',
-			'id'          => __( 'ID', 'fv-wordpress-flowplayer' ),
-            'name' => __( 'Name', 'fv-wordpress-flowplayer' ),
+			'id'          => __( 'Playlist', 'fv-wordpress-flowplayer' ),
+      'name'        => __( 'Playlist Name', 'fv-wordpress-flowplayer' ),
 			'thumbs'      => __( 'Videos', 'fv-wordpress-flowplayer' ),
 			'edit'			  => ''
 		);
