86734daab348d6594d5573b8ea3d28a80dc0f3a2
Martin Ambrus <martin@martinambrus.com>
2019-05-02 14:45:53 +0200

    refactor: moving GB init into constructor

--- models/list-table.php
+++ models/list-table.php
@@ -153,6 +153,11 @@ class FV_Player_List_Table extends WP_List_Table {
       'ajax'     => false,
     ) );
 
+    // initialize video and video meta objects, so if there are no video tables created in the DB,
+    // we'll create them now (and no SQL errors will be displayed on the listing page)
+    new FV_Player_Db_Video(-1);
+    new FV_Player_Db_Video_Meta(-1);
+
     $this->get_result_counts();
     $this->process_bulk_action();
     $this->base_url = admin_url( 'admin.php?page=fv_player' );
@@ -238,10 +243,6 @@ class FV_Player_List_Table extends WP_List_Table {
         $value .= "</div>";
         break;
       case 'embeds':
-        // initialize video and video meta objects, so if there are no video tables created in the DB,
-        // we'll create them now (and no SQL errors will be displayed on the listing page)
-        new FV_Player_Db_Video(-1);
-        new FV_Player_Db_Video_Meta(-1);
         $player = new FV_Player_Db_Player($id);
         $value = '';
         if( $player->getIsValid() ) {
@@ -279,17 +280,12 @@ class FV_Player_List_Table extends WP_List_Table {
   }
   
   public function get_result_counts() {
-	  // initialize video and video meta objects, so if there are no video tables created in the DB,
-	  // we'll create them now (and no SQL errors will be displayed on the listing page)
-	  new FV_Player_Db_Video(-1);
-	  new FV_Player_Db_Video_Meta(-1);
       $this->total_items = FV_Player_Db_Player::getTotalPlayersCount();
   }
 
   public function get_data() {
     $current = !empty($_GET['paged']) ? intval($_GET['paged']) : 1;
     $order = !empty($_GET['order']) ? esc_sql($_GET['order']) : 'desc';
-
     $order_by = !empty($_GET['orderby']) ? esc_sql($_GET['orderby']) : 'p.id';
     $single_id = !empty($_GET['id']) ? esc_sql($_GET['id']) : null;
     $search = !empty($_GET['s']) ? esc_sql($_GET['s']) : null;
