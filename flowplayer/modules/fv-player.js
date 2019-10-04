c4c28959dad206f000f35bd2d89347e58462fc32
Michal <michal.kacir@gmail.com>
2019-10-04 11:37:05 +0200

    embed: module rework

--- flowplayer/modules/fv-player.js
+++ flowplayer/modules/fv-player.js
@@ -442,6 +442,16 @@ function fv_parse_sharelink(src){
   return prefix + src;
 }
 
+function fv_player_get_video_link_hash(api) {
+  var hash = fv_parse_sharelink( typeof(api.video.sources_original) != "undefined" && typeof(api.video.sources_original[0]) != "undefined" ? api.video.sources_original[0].src : api.video.sources[0].src);
+
+  if( typeof(api.video.id) != "undefined" ) {
+    hash = fv_parse_sharelink(api.video.id.toString());
+  }
+
+  return hash;
+}
+
 function fv_player_time_hms(seconds) {
 
   if(isNaN(seconds)){
