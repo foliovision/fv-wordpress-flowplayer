8545262a550d2bcf7269dafdd882dd19bbc797f1
Michal <michalk12456@gmail.com>
2020-03-03 14:13:25 +0100

    subtitles: add comments

--- flowplayer/modules/subtitle.module.js
+++ flowplayer/modules/subtitle.module.js
@@ -1,17 +1,18 @@
 /**
- * Subtitle module
+ * Improve subtitle activation
  */
 flowplayer( function(api,root) {
   root = jQuery(root);
   var currentPoint, check = false;
 
-  api.bind('cuepoint', function(e, api, cue) {
+  api.bind('cuepoint', function(e, api, cue) { // Get current cuepoint
+    check = false;
     if (cue.subtitle) {
       currentPoint = cue.index;
     }
   });
 
-  root.on('click', '.fp-subtitle-menu', function(e) {
+  root.on('click', '.fp-subtitle-menu', function(e) { // Start checking on subtitle menu click
     check = true;
     api.on('progress', time_check);
   });
@@ -19,12 +20,11 @@ flowplayer( function(api,root) {
   // Trigger cuepoint if user enables subtitles during play 
   function time_check(e, api, time) {
     if(check) {
-      (api.cuepoints || []).forEach(function(cue, index) {
+      (api.cuepoints || []).forEach(function(cue, index) { // Find subtitle which wasnt shown
         var entry = cue.subtitle;
         if (entry && currentPoint != index) {
           if (time >= cue.time && (!entry.endTime || time <= entry.endTime)) {
             api.trigger('cuepoint', [api, cue]);
-            check = false;
           }
         }
       });
