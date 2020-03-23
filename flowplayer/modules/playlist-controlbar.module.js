cb2e7554c0fd4458b5b780bb0d1d5cab43a08eb5
Michal <michal.kacir@gmail.com>
2020-03-23 09:22:18 +0100

    playlist: add comment

--- flowplayer/modules/playlist-controlbar.module.js
+++ flowplayer/modules/playlist-controlbar.module.js
@@ -44,7 +44,7 @@ flowplayer( function(api,root) {
   });
   
   jQuery('a',playlist_menu).click( function() {
-    if(typeof(api.conf.playlist[jQuery(this).data('index') - 1]) != 'undefined' && typeof(api.conf.playlist[jQuery(this).data('index') - 1].click) != 'undefined' ) {
+    if(typeof(api.conf.playlist[jQuery(this).data('index') - 1]) != 'undefined' && typeof(api.conf.playlist[jQuery(this).data('index') - 1].click) != 'undefined' ) { // check if FV Player Pro Video Ad is in front of video - act as if clicked on Ad
       api.play(jQuery(this).data('index') - 1);
     } else {
       api.play(jQuery(this).data('index'));
