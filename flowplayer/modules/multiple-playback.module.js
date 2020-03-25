ff0dbbe01c33f77bd765a670978fdd3b372ab9c4
fvmartin <martinv@foliovision.com>
2020-03-25 13:09:53 +0100

    Multiple video playback - pause as the new default rather than unload (reset)

--- flowplayer/modules/multiple-playback.module.js
+++ flowplayer/modules/multiple-playback.module.js
@@ -1,13 +1,11 @@
 flowplayer( function(api,root) {
-  if( !api.conf.multiple_playback ) return;
-
   root = jQuery(root);
 
   var instance_id = root.data('flowplayer-instance-id');
 
   flowplayer.audible_instance = -1;
 
-  // if multiple playback is allowed, we discard the splash configuration for this instance
+  // we discard the splash configuration for each player instance
   // that way it won't get unloaded if another player starts playing
   // we need to do this once it actually starts loading, as otherwise Flowplayer would only start buffering the video
   api.one('load', function() {
@@ -21,20 +19,24 @@ flowplayer( function(api,root) {
   api.on('load', function() {
     var i = 0,
       is_muted = root.data('volume') == 0;
+      
+    jQuery('.flowplayer[data-flowplayer-instance-id]').each( function() {
+      var player = jQuery(this).data('flowplayer');
 
-    if( !is_muted ) {
-      // we go through all the players to mute them all
-      jQuery('.flowplayer[data-flowplayer-instance-id]').each( function() {
-        var player = jQuery(this).data('flowplayer');
-
-        if( player && player.playing ) {
-          player.mute(true,true);
+      if( player && player.playing ) {
+        // it multiple video playback is enabled we go through all the players to mute them all
+        // if this video is not muted
+        if( api.conf.multiple_playback && !is_muted ) {
+          player.mute(true,rue);
+        } else {
+          player.pause();
         }
-      });
+      }
+    });
 
-      // mark the current player as the one who is making the noise
-      flowplayer.audible_instance = instance_id;
-    }
+    // mark the current player as the one who is making the noise
+    flowplayer.audible_instance = instance_id;
+
 
   }).on('mute', function(e,api,muted) {
     // if the player was unmuted, mute the player which was audible previously
@@ -45,5 +47,20 @@ flowplayer( function(api,root) {
       flowplayer.audible_instance = instance_id;
     }
 
-  });
+  }).on('resume', function() {
+    // if we resume a video, we need to pause all the other ones, unless multiple playback is enabled
+    if( !api.conf.multiple_playback ) {
+      jQuery('.flowplayer[data-flowplayer-instance-id]').each( function() {
+
+        // of course skip the current player which is being resumed
+        if( instance_id == jQuery(this).data('flowplayer-instance-id') ) return;
+
+        var player = jQuery(this).data('flowplayer');
+
+        if( player && player.playing ) {
+          player.pause();
+        }
+      });
+    }
+  })
 });
