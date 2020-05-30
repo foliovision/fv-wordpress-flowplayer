00703b169bb101d0170bf399e16ebe32209f68f3
Martin Ambrus <martin@martinambrus.com>
2020-05-30 13:47:48 +0200

    fix: enqueue JS & CSS only when needed and used

--- models/media-browser.php
+++ models/media-browser.php
@@ -20,8 +20,11 @@ abstract class FV_Player_Media_Browser {
 
   function init_base() {
     global $fv_wp_flowplayer_ver;
-    wp_enqueue_script( 'flowplayer-browser-base', flowplayer::get_plugin_url().'/js/media-library-browser-base.js', array('jquery'), $fv_wp_flowplayer_ver, true );
-    wp_enqueue_style('fvwpflowplayer-s3-browser', flowplayer::get_plugin_url().'/css/s3-browser.css','','1.0','screen');
+    $screen = get_current_screen();
+    if ($screen->base == 'post' || $screen->base == 'toplevel_page_fv_player') {
+      wp_enqueue_script( 'flowplayer-browser-base', flowplayer::get_plugin_url() . '/js/media-library-browser-base.js', array( 'jquery' ), $fv_wp_flowplayer_ver, true );
+      wp_enqueue_style( 'fvwpflowplayer-s3-browser', flowplayer::get_plugin_url() . '/css/s3-browser.css', '', '1.0', 'screen' );
+    }
     $this->init();
   }
 
