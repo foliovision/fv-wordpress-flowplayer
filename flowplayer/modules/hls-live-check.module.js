185caf93b0211e6e603082edbb6bff41326bbfe9
Martin Vicenik <martinv@MartinCastle.local>
2020-06-17 10:57:59 +0200

    HLS Live status detection - improvements for Vimeo live streams

--- flowplayer/modules/hls-live-check.module.js
+++ flowplayer/modules/hls-live-check.module.js
@@ -13,42 +13,59 @@ flowplayer( function(api,root) {
   }
   
   api.on("error", function (e, api, err) {
-    if( !api.conf.clip.live ) return;
-    
-    var delay = initialDelay;
-
-    clearInterval(timer);
-
-    if (err.code === 2 || err.code === 4) {
-      root.className += " is-offline";
-
-      if (flowplayer.support.flashVideo) {
-        api.one("flashdisabled", function () {
-          root.querySelector(".fp-flash-disabled").style.display = "none";
-        });
-      }
+    setTimeout( function() {
+      if( !api.conf.clip.live &&  !api.conf.live ) return;
       
-      var messageElement = root.querySelector(".fp-ui .fp-message");
-      messageElement.innerHTML = "<h2>We are sorry, currently no live stream available.</h2><p>Retrying in <span>" + initialDelay + "</span> seconds ...</p>";
+      var delay = api.conf.clip.live_starts_in || initialDelay,
+        message = api.conf.clip.live_starts_in ? "<h2>Live stream scheduled</h2><p>Starting in <span>" + secondsToDhms(delay) + "</span>.</p>" : "<h2>We are sorry, currently no live stream available.</h2><p>Retrying in <span>" + secondsToDhms(delay) + "</span> seconds ...</p>";
 
-      timer = setInterval(function () {
-        delay -= 1;
+      clearInterval(timer);
 
-        if (delay && messageElement) {
-          messageElement.querySelector("span").innerHTML = delay;
-        } else {
-          clearInterval(timer);
-          api.error = api.loading = false;
-          if (messageElement) {
-            root.querySelector(".fp-ui").removeChild(messageElement);
-          }
-          root.className = root.className.replace(/\bis-(error|offline)\b/g, "");
-          api.load();
+      if (err.code === 2 || err.code === 4) {
+        root.className += " is-offline";
+
+        if (flowplayer.support.flashVideo) {
+          api.one("flashdisabled", function () {
+            root.querySelector(".fp-flash-disabled").style.display = "none";
+          });
         }
+        
+        var messageElement = root.querySelector(".fp-ui .fp-message");
+        messageElement.innerHTML = message;
 
-      }, 1000);
-    }
+        timer = setInterval(function () {
+          delay -= 1;
+
+          if (delay > 0 && messageElement) {
+            messageElement.querySelector("span").innerHTML = secondsToDhms(delay);
+          } else {
+            clearInterval(timer);
+            api.error = api.loading = false;
+            if (messageElement) {
+              root.querySelector(".fp-ui").removeChild(messageElement);
+            }
+            root.className = root.className.replace(/\bis-(error|offline)\b/g, "");
+            api.load();
+          }
+
+        }, 1000);
+      }
+    },1);
 
   });
+
+  function secondsToDhms(seconds) {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600*24));
+    var h = Math.floor(seconds % (3600*24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+    
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute and " : " minutes and ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+  }
   
 });
