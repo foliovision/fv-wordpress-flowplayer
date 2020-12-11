8029e634109d681beddec5a0f804737c8d0d2ba0
fvmartin <fvmartin@users.noreply.github.com>
2020-12-11 13:01:17 +0100

    Adava - front-end editing fix attempt

--- models/avada-builder-bridge.php
+++ models/avada-builder-bridge.php
@@ -1,9 +1,10 @@
 <?php
 
-add_action( 'admin_footer', 'fv_player_avada_builder_bridge' );
+add_action( 'wp_footer', 'fv_player_avada_builder_bridge', PHP_INT_MAX ); // front-end editing
+add_action( 'admin_footer', 'fv_player_avada_builder_bridge' ); // back-end editing
 
 function fv_player_avada_builder_bridge() {
-  if( did_action('fusion_builder_admin_scripts_hook') ) {
+  if( did_action('fusion_builder_admin_scripts_hook') || !empty($_GET['fb-edit']) ) { // either the admin scripts are loaded or it's the front-end editing
     ?>
 <script>
 jQuery( function($) {
@@ -14,4 +15,16 @@ jQuery( function($) {
 </script>  
     <?php
   }
+}
+
+// front-end editing
+if( !empty($_GET['fb-edit']) ) {
+  if( !function_exists('fv_wp_flowplayer_edit_form_after_editor') ) include( dirname( __FILE__ ) . '/../controller/editor.php' );
+	
+  add_action( 'wp_footer', 'fv_player_shortcode_editor_scripts_enqueue' );
+  add_action( 'wp_footer', 'fv_wp_flowplayer_edit_form_after_editor' );
+  add_action( 'wp_footer', 'flowplayer_prepare_scripts' );
+
+  // TODO: FV Player Coconut doesn't work here
+  // TODO: Shortcode is not put into the editor
 }
