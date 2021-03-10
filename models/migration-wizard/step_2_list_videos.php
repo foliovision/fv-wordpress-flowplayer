7a0054e032ba2d33d82e4bc177950ccc0cacaa93
fvmartin <martinv@foliovision.com>
2021-03-10 17:44:11 +0100

    Migration wizard - simpler yet more effective list of changes to be made

--- models/migration-wizard/step_2_list_videos.php
+++ models/migration-wizard/step_2_list_videos.php
@@ -14,7 +14,7 @@ class FV_Player_Wizard_Step_2_List_Videos extends FV_Player_Wizard_Step_Base {
     )
   ); 
 
-  public function __construct($search_string = false) {
+  public function __construct($search_string = 'amazon') {
     $this->search_string = $search_string;
   }
 
@@ -23,36 +23,42 @@ class FV_Player_Wizard_Step_2_List_Videos extends FV_Player_Wizard_Step_Base {
     ?>
 <tr>
   <td colspan="2">
-    <h2>Step 2: Replace with...</h2>
-  </td>
-</tr>
-<tr>
-  <td colspan="2">
-    <p>Videos found:</p>
-  </td>
-</tr>
+    <h2>Step 2: List of affected videos</h2>
+
     <?php
-    
-        
     if( $this->search_string ) {
       global $wpdb;
       $videos_data = $wpdb->get_results( $wpdb->prepare(
-        "SELECT id, src FROM `{$wpdb->prefix}fv_player_videos` WHERE src LIKE %s", '%' . $wpdb->esc_like($this->search_string) . '%'
+        "SELECT id, src, src1, src2, splash FROM `{$wpdb->prefix}fv_player_videos` WHERE src LIKE %s OR src1 LIKE %s OR src2 LIKE %s OR splash LIKE %s",
+        '%' . $wpdb->esc_like($this->search_string) . '%',
+        '%' . $wpdb->esc_like($this->search_string) . '%',
+        '%' . $wpdb->esc_like($this->search_string) . '%',
+        '%' . $wpdb->esc_like($this->search_string) . '%'
       ) );
       
-      if( !empty($videos_data) ) {
-        foreach($videos_data as $video) {
-      ?>
-  <tr>
-    <td colspan="2">
-      <p>ID <?php echo $video->id ?> </p>
-      <p>Src <?php echo $video->src ?> </p>
-    </td>
-  </tr>
-      <?php } ?>
+      if( !empty($videos_data) ) : ?>
+        <table class="wp-list-table widefat fixed striped logentries">
+          <thead>
+            <tr>
+              <td>Video ID</td>
+              <td>URL</td>
+              <td>Alternative URL</td>
+              <td>Alternative URL 2</td>
+              <td>Splash</td>
+            </tr>
+          </thead>
+          <?php foreach($videos_data as $video) : ?>
+            <tr>
+              <td><?php echo $video->id ?></td>
+              <td><?php echo $this->hilight($video->src); ?></td>
+              <td><?php echo $this->hilight($video->src1); ?></td>
+              <td><?php echo $this->hilight($video->src2); ?></td>
+              <td><?php echo $this->hilight($video->splash); ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
   <input type="hidden" name="search_string" value="<?php echo esc_attr($this->search_string) ?>" >
-      <?php
-      }
+      <?php endif;
       
     }
     ?>
@@ -67,6 +73,11 @@ class FV_Player_Wizard_Step_2_List_Videos extends FV_Player_Wizard_Step_Base {
       'name' => 'Replace string',
       'class' => 'regular-text code'
     ) );
+  }
+  
+  function hilight( $string ) {
+    $string = str_replace( $this->search_string, '<span style="background: #f88">'.$this->search_string.'</span>', $string );
+    return $string;
   }
 
   function process() {
