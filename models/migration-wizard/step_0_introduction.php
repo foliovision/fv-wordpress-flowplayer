667c5e2a172785e3fdee102a9699404ce26f31fb
fvmartin <martinv@foliovision.com>
2021-03-31 16:01:23 +0200

    Migration wizard - improving intro message

--- models/migration-wizard/step_0_introduction.php
+++ models/migration-wizard/step_0_introduction.php
@@ -15,7 +15,12 @@ class FV_Player_Wizard_Introduction extends FV_Player_Wizard_Step_Base_Class {
 <tr>
   <td colspan="2">
     <h2>Welcome</h2>
-    <p>This wizard will help you change the domain where your videos are hosted if you are switching CDNs. First you enter the source URL to replace and then the destination URL.</p>
+    <p>This wizard will help you change the domain where your videos are hosted if you are switching CDNs. First you enter the source URL to replace and then the destination URL. Then you see what video URLs you get.</p>
+    <p>Please note that:</p>
+    <ol>
+      <li>the wizard <strong>does not</strong> actually move any files</li>
+      <li>it only updates the FV Player database tables, <code>[fvplayer src="..."]</code> shortcodes are not affected</li>
+    </ol>
     <p>The process is divided into multiple steps with preview and a confirmation before any change is done.</p>
   </td>
 </tr>
