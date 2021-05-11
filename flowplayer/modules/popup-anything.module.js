60920ae942cf1cc4a0559f665ca329762b00c9b8
Michal <michal.kacir@gmail.com>
2021-05-11 11:18:34 +0200

    popup-anything: no players & comments

--- flowplayer/modules/popup-anything.module.js
+++ flowplayer/modules/popup-anything.module.js
@@ -1,10 +1,12 @@
-document.addEventListener('custombox:overlay:close', function(e) {
-  $players = jQuery(this).find('.flowplayer');
+document.addEventListener('custombox:overlay:close', function(e) { // popup-anything closed
+  $players = jQuery(this).find('.flowplayer'); // find players in the popup
 
-  $players.each(function(index,player){
+  if( $players.length == 0 ) return; // no players in popup
+
+  $players.each(function(index,player) { 
     var api = jQuery(player).data("flowplayer");
     if( typeof(api) != "undefined") {
-      if( api.playing ) api.pause();
+      if( api.playing ) api.pause(); // pause if playing
     }
   });
   
