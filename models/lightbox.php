fd3803baaaf23344b040216b65081f3988a19ee6
fvmartin <fvmartin@users.noreply.github.com>
2021-05-12 14:04:34 +0200

    Lightbox for images - only load if image detected in post content

--- models/lightbox.php
+++ models/lightbox.php
@@ -58,7 +58,6 @@ class FV_Player_lightbox {
     global $fv_fp, $fv_wp_flowplayer_ver;
     if(
       !$force &&
-      !$fv_fp->_get_option('lightbox_images') && // "Use video lightbox for images as well" is disabled
       !$fv_fp->_get_option('lightbox_force') // "Remove fancyBox" compatibility option is disabled
     ) return;
     
@@ -364,7 +363,12 @@ class FV_Player_lightbox {
       return $content;
     }
 
-    $content = preg_replace_callback('~(<a[^>]*?>\s*?)(<img.*?>)~', array($this, 'html_lightbox_images_callback'), $content);
+    $content = preg_replace_callback('~(<a[^>]*?>\s*?)(<img.*?>)~', array($this, 'html_lightbox_images_callback'), $content, -1, $count );
+
+    if( $count ) {
+      $this->enqueue();
+    }
+
     return $content;
   }
 
@@ -400,7 +404,6 @@ class FV_Player_lightbox {
     global $fv_fp;
     if(
       $this->should_load() ||
-      $fv_fp->_get_option('lightbox_images') || // "Use video lightbox for images as well" is enabled
       $fv_fp->should_force_load_js() || // "Load FV Flowplayer JS everywhere" is enabled
       $fv_fp->_get_option('lightbox_force') // "Remove fancyBox" compatibility option is enabled
     ) {
