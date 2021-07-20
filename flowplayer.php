5d0f017e9bd05c8c965d5fbea084e5b6e42ffda9
Michal <michalk12456@gmail.com>
2021-07-20 16:41:24 +0200

    wp: move to readme.txt

--- flowplayer.php
+++ flowplayer.php
@@ -7,7 +7,6 @@ Version: 7.5.0.727
 Author URI: http://foliovision.com/
 License: GPL-3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
-Tested up to: 5.8
 */
 
 /* FV Player - HTML5 video player with Flash fallback  
