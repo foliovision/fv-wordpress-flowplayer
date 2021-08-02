1ac2e9b4e99e39ff550960fec90a2066e3bb04ef
Martin Ambruš <martin@martinambrus.com>
2021-08-02 14:17:57 +0200

    feat: DOS Media Browser uploader

--- models/digitalocean-spaces-browser.class.php
+++ models/digitalocean-spaces-browser.class.php
@@ -5,30 +5,10 @@ if( !class_exists('FV_Player_DigitalOcean_Spaces_Browser') && class_exists('FV_P
 class FV_Player_DigitalOcean_Spaces_Browser extends FV_Player_Media_Browser {
 
   function init() {
-    global $fv_fp;
-
     if( $this->isSetUpCorrectly() ) {
       global $fv_wp_flowplayer_ver;
-
-      $endpoint = $fv_fp->_get_option(array('digitalocean_spaces','endpoint'));
-      $bucket = $fv_fp->_get_option(array('digitalocean_spaces','space'));
-      if ( strpos( $endpoint, 'http://') === false && strpos( $endpoint, 'https://') === false ) {
-        $endpoint = 'https://' . $bucket . '.' . $endpoint . ':443/';
-      }
       wp_enqueue_script( 'fv-player-dos-browser', flowplayer::get_plugin_url().'/js/digitalocean-spaces-browser.js', array( 'flowplayer-browser-base' ), $fv_wp_flowplayer_ver );
-      wp_localize_script( 'fv-player-dos-browser', 'fv_player_dos_browser', array(
-        'dos_browser_endpoint' => $endpoint,
-      ) );
     }
-  }
-
-  function register() {
-    add_action( $this->ajax_action_name, array($this, 'load_assets') );
-
-    // register extra AJAX functions for file uploads to Vzaar
-    add_action( 'wp_ajax_dos_get_signature', array($this, 'get_signature') );
-    /*add_action( 'wp_ajax_dos_upload_complete', array($this, 'upload_complete') );
-    add_action( 'wp_ajax_dos_check_progress', array($this, 'progress_check') );*/
   }
 
   // Legacy
@@ -40,146 +20,10 @@ class FV_Player_DigitalOcean_Spaces_Browser extends FV_Player_Media_Browser {
     }
   }
 
-  function get_signature() {
-    global $fv_fp;
-
-    $bucket = $fv_fp->_get_option(array('digitalocean_spaces','space'));
-    $secret = $fv_fp->_get_option(array('digitalocean_spaces','secret'));
-
-    $policy = base64_encode(json_encode(array(
-      // ISO 8601 - date('c'); generates incompatible date, so better do it manually
-      'expiration' => date('Y-m-d\TH:i:s.000\Z', strtotime('+1 day')),
-      'conditions' => array(
-        array('bucket' => $bucket),
-        array('acl' => 'public-read'),
-        array('starts-with', '$utf8', ''),
-        array('starts-with', '$key', ''),
-        array('starts-with', '$Content-Type', ''), // accept all files
-        // Plupload internally adds name field, so we need to mention it here
-        array('starts-with', '$name', ''),
-        // One more field to take into account: Filename - gets silently sent by FileReference.upload() in Flash
-        // http://docs.amazonwebservices.com/AmazonS3/latest/dev/HTTPPOSTFlash.html
-        array('starts-with', '$Filename', ''),
-      )
-    )));
-
-    $ret = array(
-      'signature' => base64_encode(hash_hmac('sha1', $policy, $secret, true)),
-      'access_key_id' => $fv_fp->_get_option(array('digitalocean_spaces','key')),
-      'policy' => $policy,
-    );
-
-    wp_send_json( $ret );
-    wp_die();
-  }
-
-  function upload_complete() {
-    $this->initVzaarSDK();
-
-    $ret = array();
-
-    if (isset($_POST["guid"])) {
-      $multi["guid"] = $_POST["guid"];
-      $multi["ingest_recipe_id"] = $_POST["ingest_recipe_id"];
-      $multi["title"] = $_POST['original_filename'];
-
-      $video = VzaarApi\Video::create($multi);
-      $ret['id'] = $video->id;
-
-      // store uploaded but unprocessed files in the DB,
-      // so we can still show them in the browser
-      $uploading = get_option('fv_player_vzaar_queue', array() );
-      $uploading[] = $video->id;
-      update_option( 'fv_player_vzaar_queue', $uploading );
-    }
-
-    wp_send_json( $ret );
-    wp_die();
-  }
-
-  function progress_check() {
-    global $FV_Player_Db;
-
-    $this->initVzaarSDK();
-
-    try {
-      $lookupFile = VzaarApi\Video::find( $_GET['id'] );
-      header( 'Content-Type: application/json' );
-      $ret = array(
-        'state'      => $lookupFile->state,
-        'renditions' => $lookupFile->renditions
-      );
-    } catch ( VzaarApi\Exceptions\VzaarException $ve ) {
-      $ret = array( 'error' => $ve->getMessage() );
-    } catch ( VzaarApi\Exceptions\VzaarError $verr ) {
-      $ret = array( 'error' => $verr->getMessage() );
-    }
-
-    // check the status and create a new player for a finished upload
-    if ( empty( $ret['error'] ) ) {
-      $generate_player = false;
-
-      if ( $ret['state'] == 'ready' ) {
-        $generate_player = true;
-      } else if ( $ret['state'] == 'failed' ) {
-        $generate_player = false;
-      } else {
-        $renditionsReady   = 0;
-        $somethingFinished = false;
-
-        foreach ( $ret['renditions'] as $rendition ) {
-          if ( $rendition->status == 'finished' || $rendition->status == 'skipped' || $rendition->status == 'failed' ) {
-            $renditionsReady ++;
-          }
-
-          if ( $rendition->status == 'finished' ) {
-            $somethingFinished = true;
-          }
-        }
-
-        // sometimes, all renditions may fail but the file will be stuck in processing,
-        // so we'll mark it as failed
-        $rCount = count( $ret['renditions'] );
-        if ( $rCount && $renditionsReady == $rCount ) {
-          if ( $somethingFinished ) {
-            $generate_player = true;
-          }
-        }
-      }
-
-      if ($generate_player ) {
-        $meta_key = 'vzaar-upload-player';
-        $meta = get_post_meta( $_GET['postID'], $meta_key, true );
-        if( !$meta ) {
-          $meta = array();
-          // only generate new player if we don't already have one
-          $player = array( 'videos' => array(), 'name' => $lookupFile->title );
-          $video = array(
-            'src' => str_replace('/video', '/adaptive.m3u8', $lookupFile->asset_url),
-            'splash' => $lookupFile->poster_url,
-            'caption' => $lookupFile->title,
-            //'meta' => array( array( 'meta_key' => 'synopsis', 'meta_value' => $synopsis[0] ) )
-          );
-
-          $player['videos'][] = $video;
-          $player_id = $FV_Player_Db->import_player_data(false, false, $player );
-
-          $meta['player_id'] = $player_id;
-          update_post_meta( $_GET['postID'], $meta_key, $meta );
-          $ret['player_id'] = $player_id;
-        } else {
-          $ret['player_id'] = $meta['player_id'] . '...';
-        }
-      }
-    }
-
-    wp_send_json( $ret );
-    wp_die();
-  }
-
   function get_s3_client() {
-    global $fv_fp, $s3Client, $FV_Player_DigitalOcean_Spaces;
+    global $fv_fp, $FV_Player_DigitalOcean_Spaces;
 
+    // instantiate the S3 client with AWS credentials
     $endpoint = 'https://' . $FV_Player_DigitalOcean_Spaces->get_endpoint();
 
     $region = $FV_Player_DigitalOcean_Spaces->get_region();
@@ -189,22 +33,17 @@ class FV_Player_DigitalOcean_Spaces_Browser extends FV_Player_Media_Browser {
 
     $credentials = new Aws\Credentials\Credentials( $key, $secret );
 
-    // instantiate the S3 client with AWS credentials
-    $s3Client = Aws\S3\S3Client::factory( array(
+    return Aws\S3\S3Client::factory( array(
       'credentials' => $credentials,
       'region'      => $region,
       'version'     => 'latest',
       'endpoint' => $endpoint
     ) );
-
-    return $s3Client;
   }
 
   function get_formatted_assets_data() {
-    global $fv_fp;
-
     $this->fv_wp_flowplayer_include_aws_sdk();
-    $s3Client = $this->get_s3_client();
+    global $fv_fp, $s3Client;
 
     $bucket = $fv_fp->_get_option(array('digitalocean_spaces','space'));
     //$domain = $fv_fp->_get_option(array('digitalocean_spaces','space'));
@@ -215,6 +54,9 @@ class FV_Player_DigitalOcean_Spaces_Browser extends FV_Player_Media_Browser {
       'path' => !empty($_POST['path']) ? $_POST['path'] : 'Home/',
       'items' => array()
     );
+
+    // instantiate the S3 client with AWS credentials
+    $s3Client = $this->get_s3_client();
 
     try {
       $args = array(
@@ -352,6 +194,7 @@ class FV_Player_DigitalOcean_Spaces_Browser extends FV_Player_Media_Browser {
 
 }
 
-new FV_Player_DigitalOcean_Spaces_Browser( 'wp_ajax_load_dos_assets' );
+global $FV_Player_DigitalOcean_Spaces_Browser;
+$FV_Player_DigitalOcean_Spaces_Browser = new FV_Player_DigitalOcean_Spaces_Browser( 'wp_ajax_load_dos_assets' );
 
 endif;
