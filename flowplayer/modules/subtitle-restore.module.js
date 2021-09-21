f8e2d081a3347c5ebd9eb99207a868088aaf45bc
fvmartin <fvmartin@users.noreply.github.com>
2021-09-21 15:43:27 +0200

    Subtitle remembering - easier to read code

--- flowplayer/modules/subtitle-restore.module.js
+++ flowplayer/modules/subtitle-restore.module.js
@@ -4,14 +4,16 @@
 flowplayer( function(api,root) {
   root = jQuery(root);
 
+  var ls = window.localStorage;
+
   // restore subtitle on ready event
   api.on('ready', function(e,api,video) {
     if( root.find('strong.fp-cc').is(":visible") ) {
-      if( window.localStorage.fv_player_subtitle && api.video.subtitles.length ) {
+      if( ls.fv_player_subtitle && api.video.subtitles.length ) {
         api.video.subtitles.forEach(function (item, index) {
-          if( item.srclang === window.localStorage.fv_player_subtitle)  {
+          if( item.srclang === ls.fv_player_subtitle)  {
             api.loadSubtitles(index);
-          } else if ( window.localStorage.fv_player_subtitle === 'none' ) {
+          } else if ( ls.fv_player_subtitle === 'none' ) {
             setTimeout(function() { // core flowplayer picks default subtitle, setTimeout is needed to prevent it
               api.disableSubtitles();
             },0);
@@ -22,13 +24,10 @@ flowplayer( function(api,root) {
 
     // subtitle menu click
     root.find('.fp-subtitle-menu').on('click', function(e) {
-      if( typeof(e.target.getAttribute('data-subtitle-index')) == 'string' ) {
+      var subtitle_index = e.target.getAttribute('data-subtitle-index');
+      if( typeof(subtitle_index) == 'string' ) {
         try {
-          if( e.target.getAttribute('data-subtitle-index') > -1  ) {
-            window.localStorage.fv_player_subtitle = api.video.subtitles[e.target.getAttribute('data-subtitle-index')].srclang; // save lang shortcut to localstorage
-          } else if( e.target.getAttribute('data-subtitle-index') == -1 ) {
-            window.localStorage.fv_player_subtitle = 'none'; // store none value to disable subtiles on ready event
-          }
+          ls.fv_player_subtitle = subtitle_index > -1 ? api.video.subtitles[subtitle_index].srclang : 'none'; // save lang shortcut to localstorage
         } catch(e) {}
       }
     });
