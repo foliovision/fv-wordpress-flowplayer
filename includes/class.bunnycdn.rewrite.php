ef3f40f76cdc4f4f970e0d17a7989571baebbd17
Michal <michal.kacir@gmail.com>
2021-11-16 10:37:52 +0100

    bunnycdn: match url

--- includes/class.bunnycdn.rewrite.php
+++ includes/class.bunnycdn.rewrite.php
@@ -4,9 +4,10 @@ if( class_exists('BunnyCDNFilter') && !class_exists('FV_Player_BunnyCDN_Rewrite'
 
   class FV_Player_BunnyCDN_Rewrite extends BunnyCDNFilter {
     public function rewrite_url( $url ) {
-      return $this->rewrite($url);
+      $directoriesRegex = implode('|', $this->directories);
+      $regex = '~(?:'. quotemeta($this->baseUrl) .')?/((?:'.$directoriesRegex.').*)~'; // custom regex to match url
+      return preg_replace_callback($regex, array(&$this, "rewriteUrl"), $url);
     }
   }
-
 
 }
