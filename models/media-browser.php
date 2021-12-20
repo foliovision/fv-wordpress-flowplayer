2a155a86e9a4c1e5d59f43b69f90b2cb9d82c8f2
fvmartin <martinv@foliovision.com>
2021-12-20 07:37:23 +0100

    Video encoder - should know to load Media Browsers

--- models/media-browser.php
+++ models/media-browser.php
@@ -12,6 +12,8 @@ abstract class FV_Player_Media_Browser {
     add_action( 'enqueue_block_editor_assets', array($this, 'init_base') ); // for Gutenberg
     add_action( 'admin_print_scripts-toplevel_page_fv_player', array($this, 'init_base'), 0 ); // wp-admin -> FV Player
     add_action( 'admin_print_scripts-widgets.php', array($this, 'init_base'), 0 ); // wp-admin -> Widgets
+
+    // TODO: Video encoder class should take care of this
     add_action( 'admin_print_scripts-fv-player_page_fv_player_coconut', array($this, 'init_base'), 0 ); // wp-admin -> FV Player -> Encode video
     add_action( 'fv_player_media_browser_enqueue_base_uploader_css', array( $this, 'include_base_uploader_css' ) );
 
