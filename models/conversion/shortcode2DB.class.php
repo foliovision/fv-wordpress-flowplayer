7ba6f160fdf4bc8e072ae7cc5b07607a10043989
fvmartin <fvmartin@users.noreply.github.com>
2022-01-27 13:52:46 +0100

    conversion: filter trash post type in sql - fix

--- models/conversion/shortcode2DB.class.php
+++ models/conversion/shortcode2DB.class.php
@@ -22,7 +22,7 @@ class FV_Player_Shortcode2Database_Conversion extends FV_Player_Conversion_Base 
   function get_count() {
     global $wpdb;
 
-    $count = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->posts} WHERE post_status != 'inherit' AND (post_content LIKE " . implode(' OR post_content LIKE ',$this->matchers) . ") AND post_type NOT IN ('topic','reply','trash')" );
+    $count = $wpdb->get_var( "SELECT COUNT(*) FROM {$wpdb->posts} WHERE post_status NOT IN ('inherit','trash') AND (post_content LIKE " . implode(' OR post_content LIKE ',$this->matchers) . ") AND post_type NOT IN ('topic','reply')" );
 
     return intval($count);
   }
@@ -35,7 +35,7 @@ class FV_Player_Shortcode2Database_Conversion extends FV_Player_Conversion_Base 
   function get_posts_with_shortcode($offset, $limit) {
     global $wpdb;
 
-    $results = $wpdb->get_results( "SELECT ID, post_author, post_date_gmt, post_status, post_title, post_type, post_content FROM {$wpdb->posts} WHERE post_status != 'inherit' AND (post_content LIKE " . implode(' OR post_content LIKE ', $this->matchers) . ") AND post_type NOT IN ('topic','reply','trash') ORDER BY post_date_gmt DESC LIMIT {$offset},{$limit}");
+    $results = $wpdb->get_results( "SELECT ID, post_author, post_date_gmt, post_status, post_title, post_type, post_content FROM {$wpdb->posts} WHERE post_status NOT IN ('inherit','trash') AND (post_content LIKE " . implode(' OR post_content LIKE ', $this->matchers) . ") AND post_type NOT IN ('topic','reply') ORDER BY post_date_gmt DESC LIMIT {$offset},{$limit}");
 
     return $results;
   }
