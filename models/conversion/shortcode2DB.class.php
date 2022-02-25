84e5f4950ec52b5efa385a243e334f652850471b
fvmartin <martinv@foliovision.com>
2022-02-25 19:58:56 +0100

    Conversion - Shortcode2DB - refactor for postmeta

--- models/conversion/shortcode2DB.class.php
+++ models/conversion/shortcode2DB.class.php
@@ -12,6 +12,29 @@ class FV_Player_Shortcode2Database_Conversion extends FV_Player_Conversion_Base 
       ),
       'help' => __("This converts the <code>[fvplayer src=...]</code> and <code>[flowplayer src=...]</code> shortcodes into database <code>[fvplayer id=...]</code> shortcodes.", 'fv-wordpress-flowplayer')
     ) );
+
+    // atts for video that are supported
+    $this->supported_video_atts = array(
+      'src',
+      'src1',
+      'src2',
+      'splash',
+    );
+
+    // atts for player that are supported
+    $this->supported_player_atts = array(
+      'width',
+      'height',
+      'autoplay',
+      'lightbox',
+      'playlist',
+      'caption',
+      'subtitles',
+      'ab',
+      'speed'
+    );
+
+    $this->supported_atts = array_merge( $this->supported_video_atts, $this->supported_player_atts );
   }
 
   /**
@@ -55,220 +78,16 @@ class FV_Player_Shortcode2Database_Conversion extends FV_Player_Conversion_Base 
       // match shortcodes in post_content
       preg_match_all( '~\[(?:flowplayer|fvplayer).*?\]~', $post->post_content, $matched_shortcodes );
 
-      // atts for video that are supported
-      $supported_video_atts = array(
-        'src',
-        'src1',
-        'src2',
-        'splash',
-      );
-
-      // atts for player that are supported
-      $supported_player_atts = array(
-        'width',
-        'height',
-        'autoplay',
-        'lightbox',
-        'playlist',
-        'caption',
-        'subtitles'
-      );
-
-      $supported_atts = array_merge( $supported_video_atts, $supported_player_atts );
-
       if( !empty($matched_shortcodes) ) {
         foreach( $matched_shortcodes[0] as $shortcode ) {
-          $atts = shortcode_parse_atts( trim(rtrim($shortcode,']')) );
-          $import_video_atts = array();
-          $import_player_atts = array();
+          $result = $this->worker( $shortcode, $post, $new_content );
+          if( $result ) {
 
-          $import_atts = array();
+            $new_content = $result['new_content'];
+            $errors = array_merge( $errors, $result['errors'] );
 
-          unset( $atts[0] ); // remove [fvplayer or [flowplayer
-
-          // ignore db players
-          if ( isset( $atts['id'] ) ) {
-            continue;
+            $output_data[] = $result['data'];
           }
-
-          // check for unsupported args
-          $unsupported_atts_found = array();
-          foreach( $atts as $k => $v ) {
-            if( !in_array( $k, $supported_atts ) ) {
-              $unsupported_atts_found[] = $k;
-            } else {
-              $import_atts[$k] = $v;
-            }
-          }
-
-          $output_msg = "Conversion failed.";
-          $failed_msg = "";
-
-          // check if unsupported args found
-          if( !empty($unsupported_atts_found) ) {
-            $failed_msg = "Unsupported argument(s) " . implode(',', $unsupported_atts_found);
-            
-            $errors[] = array(
-              'ID' => $post->ID,
-              'post_title' => $post->post_title,
-              'post_link' => get_permalink( $post->ID ),
-              'post_edit' => get_edit_post_link( $post->ID ),
-              'shortcode' => $shortcode,
-              'message' => $failed_msg
-            );
-          } else {
-            // single video set 0 index
-            $video_index = 0;
-            $import_video_atts[$video_index] = array();
-            // add video atts
-            foreach( $supported_video_atts as $video_att ) { 
-              if( isset( $import_atts[$video_att] ) ) {
-                $import_video_atts[$video_index][$video_att] = $import_atts[$video_att];
-              }
-            }
-
-            // add player atts , parse lightbox, playlist items
-            foreach( $supported_player_atts as $player_att ) {
-              $video_index = 0; // reset video index to 0
-              if( isset( $import_atts[$player_att] ) ) {
-                if( strcmp( $player_att, 'lightbox' ) == 0 ) { // handle lightbox
-                  $lightbox_atts = explode(';', $import_atts[$player_att] );
-
-                  $lightbox_count = count( $lightbox_atts );
-
-                  if( $lightbox_count == 2 ) { // lightbox_caption
-                    $import_player_atts['lightbox_caption'] = $lightbox_atts[1];
-                  } else if( $lightbox_count == 3 ) { // lightbox_width, lightbox_height
-                    $import_player_atts['lightbox_width'] = $lightbox_atts[1];
-                    $import_player_atts['lightbox_height'] = $lightbox_atts[2];
-                  } else if( $lightbox_count == 4 ) { // lightbox_caption, lightbox_width, lightbox_height
-                    $import_player_atts['lightbox_width'] = $lightbox_atts[1];
-                    $import_player_atts['lightbox_height'] = $lightbox_atts[2];
-                    $import_player_atts['lightbox_caption'] = $lightbox_atts[3];
-                  }
-
-                  $import_player_atts[$player_att] = $lightbox_atts[0]; // lightbox
-                } else if( strcmp( $player_att, 'playlist' ) == 0 ) { // handle playlist
-                  $playlist_items = explode(';', $import_atts[$player_att]);
-                  foreach($playlist_items as $item) {
-                    $video_index++;
-                    $import_video_atts[$video_index] = array();
-
-                    $item_data = explode(',', $item );
-
-                    // parse src, scr1, scr2 and splash
-                    if( count( $item_data ) > 1 ) {
-                      foreach(  $item_data as $i => $data ) {
-                        if( preg_match('~\.(png|gif|jpg|jpe|jpeg)($|\?)~', $data) || stripos($data, 'i.vimeocdn.com') !== false ) { // splash
-                          $import_video_atts[$video_index]['splash'] = $data;
-                        } else if( $i > 0 ) { // src1, src2
-                          $import_video_atts[$video_index]['src'.$i] = $data;
-                        }
-                      }
-                    }
-
-                    $import_video_atts[$video_index]['src'] = $item_data[0]; // src
-                  }
-                } else if(strcmp( $player_att, 'subtitles' ) == 0) { // subtitles
-                  $subtitles = explode(';', $import_atts[$player_att]);
-                  foreach( $subtitles as $subtile ) {
-                    if( !empty( $subtile ) ) {
-                      if( !isset( $import_video_atts[$video_index]['meta'] ) ) {
-                        $import_video_atts[$video_index]['meta'] = array();
-                      }
-                      $import_video_atts[$video_index]['meta'][] = array( 'meta_key' => 'subtitles', 'meta_value' => esc_url_raw($subtile) );
-                    }
-
-                    $video_index++;
-                  }
-
-                } else if(strcmp( $player_att, 'caption' ) == 0) { // caption
-                  $replace_from = array('&amp;','\;', '\,'); // escaped characters
-                  $replace_to = array('<!--amp-->','<!--semicolon-->','<!--comma-->'); // temp replacements
-                  $unescaped = array( '&', ';', ',' );
-                  $captions = str_replace( $replace_from, $replace_to, $import_atts[$player_att] );
-                  $captions = explode(';', $captions);
-
-                  foreach( $captions as $caption ) {
-                    $caption = str_replace( $replace_to, $unescaped, $caption );
-                    $import_video_atts[$video_index]['caption'] = $caption;
-
-                    $video_index++;
-                  }
-                } else { // other atts
-                  $import_player_atts[$player_att] =  $import_atts[$player_att];
-                }
-              }
-            }
-
-            // add metadata
-            $import_player_atts['meta'] = array( 
-              array(
-                'meta_key' => 'post_id',
-                'meta_value' => $post->ID
-              ),
-              array(
-                'meta_key' => 'fv_player_conversion',
-                'meta_value' => self::class
-              )
-            );
-
-            // add author
-            $import_player_atts['author'] = $post->post_author;
-
-            // add status
-            $import_player_atts['status'] = 'published';
-
-            // add date_created
-            $import_player_atts['date_created'] = $post->post_date_gmt;
-
-            // add player_name
-            // $import_player_atts['player_name'] = 'player_name';
-
-            // add videos
-            $import_player_atts['videos'] = $import_video_atts;
-
-            // echo '<pre>';
-            // var_export($import_player_atts);
-            // echo '</pre>';
-            // die();
-
-            if( $this->is_live() ) {
-              global $FV_Player_Db;
-              $player_id =  $FV_Player_Db->import_player_data(false, false, $import_player_atts);
-
-              if( $player_id > 0 ) {
-                // echo "Inserted player #".$player_id."\n";
-                $new_content = str_replace( $shortcode , '[fvplayer id="'.$player_id.'"]', $new_content );
-                $content_updated = true;
-                $output_msg = "New FV Player #" . $player_id ;
-              } else {
-                $failed_msg = "Error saving FV Player instance";
-
-                $errors[] = array(
-                  'ID' => $post->ID,
-                  'post_title' => $post->post_title,
-                  'post_link' => get_permalink( $post->ID ),
-                  'post_edit' => get_edit_post_link( $post->ID ),
-                  'shortcode' => $shortcode,
-                  'message' => $failed_msg
-                );
-              }
-              
-            } else {
-              $output_msg = "Would create new FV Player";
-            }
-          }
-
-          $output_data[] = array(
-            'timing' => number_format(microtime(true) - $start),
-            'ID' => $post->ID,
-            'title' => $post->post_title,
-            'type' => $post->post_type,
-            'shortcode' => $shortcode,
-            'output' => $output_msg,
-            'error' => $failed_msg
-          );
         }
       }
     }
@@ -292,6 +111,203 @@ class FV_Player_Shortcode2Database_Conversion extends FV_Player_Conversion_Base 
         </td>
       </tr>
     <?php
+  }
+
+  function worker( $shortcode, $post, $new_content ) {
+    $atts = shortcode_parse_atts( trim(rtrim($shortcode,']')) );
+    $import_video_atts = array();
+    $import_player_atts = array();
+
+    $import_atts = array();
+
+    unset( $atts[0] ); // remove [fvplayer or [flowplayer
+
+    // ignore db players
+    if ( isset( $atts['id'] ) ) {
+      return false;
+    }
+
+    // check for unsupported args
+    $unsupported_atts_found = array();
+    foreach( $atts as $k => $v ) {
+      if( !in_array( $k, $this->supported_atts ) ) {
+        $unsupported_atts_found[] = $k;
+      } else {
+        $import_atts[$k] = $v;
+      }
+    }
+
+    $output_msg = "Conversion failed.";
+    $failed_msg = "";
+
+    // check if unsupported args found
+    if( !empty($unsupported_atts_found) ) {
+      $failed_msg = "Unsupported argument(s) " . implode(',', $unsupported_atts_found);
+
+      $errors[] = array(
+        'ID' => $post->ID,
+        'post_title' => $post->post_title,
+        'post_link' => get_permalink( $post->ID ),
+        'post_edit' => get_edit_post_link( $post->ID ),
+        'shortcode' => $shortcode,
+        'message' => $failed_msg
+      );
+    } else {
+      // single video set 0 index
+      $video_index = 0;
+      $import_video_atts[$video_index] = array();
+      // add video atts
+      foreach( $this->supported_video_atts as $video_att ) { 
+        if( isset( $import_atts[$video_att] ) ) {
+          $import_video_atts[$video_index][$video_att] = $import_atts[$video_att];
+        }
+      }
+
+      // add player atts , parse lightbox, playlist items
+      foreach( $this->supported_player_atts as $player_att ) {
+        $video_index = 0; // reset video index to 0
+        if( isset( $import_atts[$player_att] ) ) {
+          if( strcmp( $player_att, 'lightbox' ) == 0 ) { // handle lightbox
+            $lightbox_atts = explode(';', $import_atts[$player_att] );
+
+            $lightbox_count = count( $lightbox_atts );
+
+            if( $lightbox_count == 2 ) { // lightbox_caption
+              $import_player_atts['lightbox_caption'] = $lightbox_atts[1];
+            } else if( $lightbox_count == 3 ) { // lightbox_width, lightbox_height
+              $import_player_atts['lightbox_width'] = $lightbox_atts[1];
+              $import_player_atts['lightbox_height'] = $lightbox_atts[2];
+            } else if( $lightbox_count == 4 ) { // lightbox_caption, lightbox_width, lightbox_height
+              $import_player_atts['lightbox_width'] = $lightbox_atts[1];
+              $import_player_atts['lightbox_height'] = $lightbox_atts[2];
+              $import_player_atts['lightbox_caption'] = $lightbox_atts[3];
+            }
+
+            $import_player_atts[$player_att] = $lightbox_atts[0]; // lightbox
+          } else if( strcmp( $player_att, 'playlist' ) == 0 ) { // handle playlist
+            $playlist_items = explode(';', $import_atts[$player_att]);
+            foreach($playlist_items as $item) {
+              $video_index++;
+              $import_video_atts[$video_index] = array();
+
+              $item_data = explode(',', $item );
+
+              // parse src, scr1, scr2 and splash
+              if( count( $item_data ) > 1 ) {
+                foreach(  $item_data as $i => $data ) {
+                  if( preg_match('~\.(png|gif|jpg|jpe|jpeg)($|\?)~', $data) || stripos($data, 'i.vimeocdn.com') !== false ) { // splash
+                    $import_video_atts[$video_index]['splash'] = $data;
+                  } else if( $i > 0 ) { // src1, src2
+                    $import_video_atts[$video_index]['src'.$i] = $data;
+                  }
+                }
+              }
+
+              $import_video_atts[$video_index]['src'] = $item_data[0]; // src
+            }
+          } else if(strcmp( $player_att, 'subtitles' ) == 0) { // subtitles
+            $subtitles = explode(';', $import_atts[$player_att]);
+            foreach( $subtitles as $subtile ) {
+              if( !empty( $subtile ) ) {
+                if( !isset( $import_video_atts[$video_index]['meta'] ) ) {
+                  $import_video_atts[$video_index]['meta'] = array();
+                }
+                $import_video_atts[$video_index]['meta'][] = array( 'meta_key' => 'subtitles', 'meta_value' => esc_url_raw($subtile) );
+              }
+
+              $video_index++;
+            }
+
+          } else if(strcmp( $player_att, 'caption' ) == 0) { // caption
+            $replace_from = array('&amp;','\;', '\,'); // escaped characters
+            $replace_to = array('<!--amp-->','<!--semicolon-->','<!--comma-->'); // temp replacements
+            $unescaped = array( '&', ';', ',' );
+            $captions = str_replace( $replace_from, $replace_to, $import_atts[$player_att] );
+            $captions = explode(';', $captions);
+
+            foreach( $captions as $caption ) {
+              $caption = str_replace( $replace_to, $unescaped, $caption );
+              $import_video_atts[$video_index]['caption'] = $caption;
+
+              $video_index++;
+            }
+          } else { // other atts
+            $import_player_atts[$player_att] =  $import_atts[$player_att];
+          }
+        }
+      }
+
+      // add metadata
+      $import_player_atts['meta'] = array( 
+        array(
+          'meta_key' => 'post_id',
+          'meta_value' => $post->ID
+        ),
+        array(
+          'meta_key' => 'fv_player_conversion',
+          'meta_value' => self::class
+        )
+      );
+
+      // add author
+      $import_player_atts['author'] = $post->post_author;
+
+      // add status
+      $import_player_atts['status'] = 'published';
+
+      // add date_created
+      $import_player_atts['date_created'] = $post->post_date_gmt;
+
+      // add player_name
+      // $import_player_atts['player_name'] = 'player_name';
+
+      // add videos
+      $import_player_atts['videos'] = $import_video_atts;
+
+      // echo '<pre>';
+      // var_export($import_player_atts);
+      // echo '</pre>';
+      // die();
+
+      if( $this->is_live() ) {
+        global $FV_Player_Db;
+        $player_id =  $FV_Player_Db->import_player_data(false, false, $import_player_atts);
+
+        if( $player_id > 0 ) {
+          // echo "Inserted player #".$player_id."\n";
+          $new_content = str_replace( $shortcode , '[fvplayer id="'.$player_id.'"]', $new_content );
+          $content_updated = true;
+          $output_msg = "New FV Player #" . $player_id ;
+        } else {
+          $failed_msg = "Error saving FV Player instance";
+
+          $errors[] = array(
+            'ID' => $post->ID,
+            'post_title' => $post->post_title,
+            'post_link' => get_permalink( $post->ID ),
+            'post_edit' => get_edit_post_link( $post->ID ),
+            'shortcode' => $shortcode,
+            'message' => $failed_msg
+          );
+        }
+
+      } else {
+        $output_msg = "Would create new FV Player";
+      }
+    }
+
+    return array(
+      'errors' => $errors,
+      'data' => array(
+        'ID' => $post->ID,
+        'title' => $post->post_title,
+        'type' => $post->post_type,
+        'shortcode' => $shortcode,
+        'output' => $output_msg,
+        'error' => $failed_msg
+      ),
+      'new_content' => $new_content
+    );
   }
 
 }
