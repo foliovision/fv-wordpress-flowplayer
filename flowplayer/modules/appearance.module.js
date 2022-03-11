9b5d97e61844aa646bb8f6acba54586ff396a399
fvmartin <fvmartin@users.noreply.github.com>
2022-03-11 15:10:10 +0100

    iOS font loading JS detection - no luck

--- flowplayer/modules/appearance.module.js
+++ flowplayer/modules/appearance.module.js
@@ -71,7 +71,54 @@ flowplayer(function(api, root) {
   api.on('unload pause finish error',function(){
     if(typeof(checker) !== 'undefined')
       clearInterval(checker);
-  })
+  });
+
+  // iOS shows Emoji for the player icons before the Flowplayer font is loaded, if that font is not loaded in less than 2 seconds
+  // Here we detect if all the fonts are loaded once the icons become visible - on playback
+  if( flowplayer.support.iOS && document.fonts && document.fonts.ready ) {    
+    api.on('ready', function(e) {
+      
+      // This fails to measure the text size
+      /*setTimeout( function() {
+        var icons = root.find('.fp-icon:visible');
+        icons.css('visibility','hidden');
+
+        icons.eq(0).fontSpy({
+          onLoad: 'hideMe',
+          onFail: 'fontFail anotherClass'
+        });
+      }, 0 );*/
+
+      // No matter what, this seems to report the font is ready even if it's not true
+      /*setTimeout( function() {
+        var icons = root.find('.fp-icon:visible');
+        icons.css('visibility','hidden');
+
+        
+        var int = setInterval( function() {
+          console.log( 'document.fonts.check("12px flowplayer", "\e002")', document.fonts.check("12px flowplayer", "\e002") );
+          if( document.fonts.check("12px flowplayer", "e002") ) {
+            icons.css('visibility', 'visible');
+            clearInterval(int);
+          }
+        }, 250 );
+      }, 0 );*/
+      
+      // No matter what, the check below all fonts are loaded even if it's not true
+      //api.one('progress', function(e) {
+      setTimeout( function() {
+        var icons = root.find('.fp-icon:visible');
+        icons.css('visibility','hidden');
+
+        // Show the icons once the fonts are ready
+        document.fonts.ready.then(function() {
+          console.log('fonts loaded 1');
+          icons.css('visibility', 'visible');
+
+        });
+      }, 0 );
+    });
+  }
 })
 
 
@@ -153,3 +200,63 @@ jQuery(document).on("tabsactivate", '.fv_flowplayer_tabs_content', function(even
   var api = objPlayer.data('flowplayer');
   api.load();
 });
+
+(function($) {
+
+  $.fontSpy = function( element, conf ) {
+      var $element = $(element);
+      var defaults = {
+          font: 'flowplayer',
+          onLoad: '',
+          onFail: '',
+          testFont: 'Comic Sans MS',
+          testString: '&#xe002',
+          delay: 50,
+          timeOut: 2500
+      };
+      var config = $.extend( defaults, conf );
+      var tester = document.createElement('span');
+          tester.style.position = 'absolute';
+          //tester.style.top = '-9999px';
+          //tester.style.left = '-9999px';
+          //tester.style.visibility = 'hidden';
+          tester.style.fontFamily = config.testFont;
+          tester.style.fontSize = '250px';
+          tester.innerHTML = config.testString;
+      document.body.appendChild(tester);
+      var fallbackFontWidth = tester.offsetWidth;
+      tester.style.fontFamily = config.font + ',' + config.testFont;
+      function checkFont() {
+        console.log('checkFont',fallbackFontWidth,loadedFontWidth);
+          var loadedFontWidth = tester.offsetWidth;
+          if (fallbackFontWidth === loadedFontWidth){
+              if(config.timeOut < 0) {
+                  $element.removeClass(config.onLoad);
+                  $element.addClass(config.onFail);
+                  console.log('failure');
+              }
+              else {
+                  console.log('have the font!');
+                  $element.addClass(config.onLoad);
+                  
+                  setTimeout(checkFont, config.delay);
+                  config.timeOut = config.timeOut - config.delay;
+              }
+          }
+          else {
+              $element.removeClass(config.onLoad);
+          }
+      }
+      checkFont();
+  };
+
+  $.fn.fontSpy = function(config) {
+      return this.each(function() {
+          if (undefined == $(this).data('fontSpy')) {
+              var plugin = new $.fontSpy(this, config);
+              $(this).data('fontSpy', plugin);
+          }
+      });
+  };
+
+})(jQuery);
