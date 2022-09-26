8cdbb13ca7401fe46d10c07853a516f84dc101b0
fvmartin <fvmartin@users.noreply.github.com>
2022-09-26 10:59:30 +0200

    Freedom Player 7.2.12

--- flowplayer.php
+++ flowplayer.php
@@ -28,7 +28,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 
 global $fv_wp_flowplayer_ver;
 $fv_wp_flowplayer_ver = '7.5.29.7210';
-$fv_wp_flowplayer_core_ver = '7.2.11';
+$fv_wp_flowplayer_core_ver = '7.2.12';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
   include_once( dirname( __FILE__ ) . '/includes/module.php' );
