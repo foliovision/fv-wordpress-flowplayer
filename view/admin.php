084a92795d8f36fd08e85b2bb0b62885be50b0a0
Michal <michal.kacir@gmail.com>
2023-03-07 14:06:19 +0100

    autoplay: fix unchecked setting

--- view/admin.php
+++ view/admin.php
@@ -505,7 +505,7 @@ function fv_flowplayer_admin_autoplay_and_preloading() {
           $id = 'autoplay_preload_'.esc_attr($key);
 
           $radio_button = '<input id="'.$id.'" type="radio" name="autoplay_preload" value="'.esc_attr($key).'"';
-          if( $value === $key ) {
+          if( json_encode($value) == $key ) { // use json_encode as value can be boolean
             $radio_button .= ' checked="checked"';
           }
           $radio_button .= '</input>';
