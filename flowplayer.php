0fe81411b80ef1a5cc738191d959fa091427aa31
fvmartin <fvmartin@users.noreply.github.com>
2023-03-07 13:19:55 +0100

    Gulp

--- flowplayer.php
+++ flowplayer.php
@@ -30,7 +30,7 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 */
 
 global $fv_wp_flowplayer_ver;
-$fv_wp_flowplayer_ver = '7.9.3.6';
+$fv_wp_flowplayer_ver = '7.9.3.7';
 $fv_wp_flowplayer_core_ver = '7.2.14.5';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
