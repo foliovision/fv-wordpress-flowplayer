873330d10d626d91dcb5d9043ed2e777f5028a55
fvmartin <fvmartin@users.noreply.github.com>
2023-03-08 15:10:03 +0100

    Stats: Better check of player properties

--- flowplayer/modules/stats.module.js
+++ flowplayer/modules/stats.module.js
@@ -5,30 +5,31 @@ flowplayer( function(api,root) {
   var last_tracked = -1; // store video index to check if video was tracked already
   
   if( !api.conf.fv_stats || !api.conf.fv_stats.enabled && ( !root.data('fv_stats') || root.data('fv_stats') == 'no' ) ) return;
+
+  try {
+    var data = root.data('fv_stats_data');
+    if ( !data ) {
+      return;
+    }
+  } catch(e) {
+    return false;
+  }
   
   api.on('ready finish', function(e,api) { // first play and replay
     api.one('progress', function(e,api) {
-      if( root.data('fv_stats_data') ) {
-        try {
-          var player_post_data = root.data('fv_stats_data');
-        } catch(e) {
-          return false;
-        }
+      // each video should be only tracked once!
+      if( last_tracked == get_index() ) return;
 
-        // each video should be only tracked once!
-        if( last_tracked == get_index() ) return;
+      last_tracked = get_index();
 
-        last_tracked = get_index();
-
-        jQuery.post( api.conf.fv_stats.url, {
-          'blog_id' : api.conf.fv_stats.blog_id,
-          'video_id' : api.video.id ? api.video.id : 0,
-          'player_id': player_post_data.player_id,
-          'post_id' : player_post_data.post_id,
-          'user_id' : api.conf.fv_stats.user_id,
-          'tag' : 'play'
-        } );
-      }
+      jQuery.post( api.conf.fv_stats.url, {
+        'blog_id' : api.conf.fv_stats.blog_id,
+        'video_id' : api.video.id ? api.video.id : 0,
+        'player_id': data.player_id,
+        'post_id' : data.post_id,
+        'user_id' : api.conf.fv_stats.user_id,
+        'tag' : 'play'
+      } );
     });
   }).on('finish', function() {
     last_tracked = -1; // reset on finish to allow tracking video again
