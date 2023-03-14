11bead4907c4eb7e8d6ee9e98913382e1641b30f
Michal <michal.kacir@gmail.com>
2023-03-14 15:50:23 +0100

    stats export: use nonce

--- models/stats-export.php
+++ models/stats-export.php
@@ -6,29 +6,32 @@ if( !class_exists('FV_Player_Stats_Export') ) :
 class FV_Player_Stats_Export {
 
   public function __construct() {
-    if( isset( $_GET['fv-stats-export-user'] ) ) {
+    if( isset( $_GET['fv-stats-export-user']) ) {
       add_action('admin_init', array( $this, 'export_user_data' ) );
     }
   }
 
   public function export_user_data() {
-    global $wpdb;
+    if( isset($_GET['nonce'] ) && wp_verify_nonce( $_GET['nonce'], 'fv-stats-export-user-' . intval($_GET['fv-stats-export-user']) ) ) {
+      global $wpdb;
 
-    $user_id = intval($_GET['fv-stats-export-user']);
+      $user_id = intval($_GET['fv-stats-export-user']);
 
-    $query = $wpdb->prepare( "SELECT user_email, date, pl.id AS player_id, src, post_title, play, seconds, duration
-      FROM `{$wpdb->prefix}fv_player_stats` AS s
-      JOIN `{$wpdb->users}` AS u ON s.user_id = u.ID
-      JOIN `{$wpdb->posts}` AS p ON s.id_post = p.ID
-      JOIN `{$wpdb->prefix}fv_player_videos` AS v ON s.id_video = v.id
-      JOIN `{$wpdb->prefix}fv_player_players` AS pl ON FIND_IN_SET( v.id, pl.videos )
-      WHERE u.ID = %d
-      ORDER BY s.id DESC",
-    $user_id);
+      $query = $wpdb->prepare( "SELECT user_email, date, pl.id AS player_id, src, post_title, play, seconds, duration
+        FROM `{$wpdb->prefix}fv_player_stats` AS s
+        JOIN `{$wpdb->users}` AS u ON s.user_id = u.ID
+        JOIN `{$wpdb->posts}` AS p ON s.id_post = p.ID
+        JOIN `{$wpdb->prefix}fv_player_videos` AS v ON s.id_video = v.id
+        JOIN `{$wpdb->prefix}fv_player_players` AS pl ON FIND_IN_SET( v.id, pl.videos )
+        WHERE u.ID = %d
+        ORDER BY s.id DESC",
+      $user_id);
 
-    $results = $wpdb->get_results( $query , ARRAY_A);
+      $results = $wpdb->get_results( $query , ARRAY_A);
 
-    $this->serve_csv($results, $user_id);
+      $this->serve_csv($results, $user_id);
+    }
+
   }
 
   private function serve_csv( $data, $user_id ) {
