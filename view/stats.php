c91b4f95d245dab09997b5038f8dbdfb56500173
fvmartin <martinv@foliovision.com>
2023-04-04 22:15:23 +0200

    Stats: Fix messages if no data

--- view/stats.php
+++ view/stats.php
@@ -290,7 +290,7 @@
     );
   });
   </script>
-<?php else : ?>
+<?php elseif ( ( strcmp($current_page, 'fv_player_stats') === 0 ) ) : ?>
   <p>There are no video stats recorded. Please note that the stats take 5-10 minutes to update.</p>
 <?php endif;?>
 
@@ -367,6 +367,8 @@ jQuery( document ).ready(function() {
   );
 })
 </script>
+<?php elseif ( ( strcmp($current_page, 'fv_player_stats_users') === 0 ) ) : ?>
+  <p>There are no video stats recorded. Please note that the stats take 5-10 minutes to update.</p>
 <?php endif; ?>
 
 <?php if( isset($fv_user_watch_time_stats_data) && !empty($fv_user_watch_time_stats_data) ): ?>
