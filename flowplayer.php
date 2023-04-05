6fdf14b0e562ba9b769f654cf737e59653c3ac83
fvmartin <fvmartin@users.noreply.github.com>
2023-04-05 14:57:40 +0200

    Min.js

--- flowplayer.php
+++ flowplayer.php
@@ -27,8 +27,8 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 */
 
 global $fv_wp_flowplayer_ver;
-$fv_wp_flowplayer_ver = '7.5.32.7212';
-$fv_wp_flowplayer_core_ver = '7.2.12.1';
+$fv_wp_flowplayer_ver = '7.5.32.7212.1';
+$fv_wp_flowplayer_core_ver = '7.2.12.2';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
   include_once( dirname( __FILE__ ) . '/includes/module.php' );
