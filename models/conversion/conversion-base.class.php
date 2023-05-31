d34281a65c429aa1c46955cf5d2ec73ced64dfc4
Michal <michal.kacir@gmail.com>
2023-05-31 11:52:15 +0200

    conversion: refactor use abstract iteration & html build

--- models/conversion/conversion-base.class.php
+++ models/conversion/conversion-base.class.php
@@ -9,6 +9,7 @@ abstract class FV_Player_Conversion_Base {
   protected $slug = 'conversion-slug';
   protected $screen;
   protected $help;
+  protected $screen_fields = array();
 
   abstract function convert_one($post);
 
@@ -17,6 +18,10 @@ abstract class FV_Player_Conversion_Base {
   abstract function get_count();
 
   abstract function conversion_button();
+
+  abstract function iterate_data( $data );
+
+  abstract function build_output_html( $data , $percent_done);
 
   function __construct( $args ) {
     $this->title = $args['title'];
@@ -29,9 +34,6 @@ abstract class FV_Player_Conversion_Base {
     add_action( 'wp_ajax_'. $this->screen, array( $this, 'ajax_convert') );
     add_action( 'fv_player_conversion_buttons', array( $this, 'conversion_button') );
 
-    if( isset($_GET['fv-conversion-export']) && !empty($_GET['page']) && $_GET['page'] === $this->screen ) {
-      add_action('admin_init', array( $this, 'csv_export' ) );
-    }
   }
 
   function admin_page() {
@@ -41,15 +43,12 @@ abstract class FV_Player_Conversion_Base {
 
   function ajax_convert() {
     if ( current_user_can( 'install_plugins' ) && check_ajax_referer( $this->screen ) ) {
-      
       if( function_exists( 'FV_Player_Pro' ) ) {
         // takes too long to save if not removed
         remove_filter( 'content_save_pre', array( FV_Player_Pro(), 'save_post' ), 10 );
       }
 
-      $conversions_output = array();
       $convert_error = false;
-      $html = array();
 
       $offset = intval($_POST['offset']);
       $offset = 0 + intval($_POST['offset2']) + $offset;
@@ -59,37 +58,17 @@ abstract class FV_Player_Conversion_Base {
 
       $total = $this->get_count();
 
-      foreach( $posts AS $post ) {
-        $result = $this->convert_one($post);
-        // mark post if conversion failed
-        if( !empty( $result['errors'] ) ) {
-          update_post_meta( $post->ID, '_fv_player_' . $this->slug . '_failed', $result['errors'] );
-          $convert_error = true;
-        } else {
-          if( $result['content_updated'] ) {
-            // no problem, unmark
-            delete_post_meta( $post->ID, '_fv_player_' . $this->slug . '_failed' );
-          }
-        }
+      // iterate data
+      $result = $this->iterate_data( $posts );
 
-        $conversions_output = array_merge( $conversions_output, $result['output_data'] );
-
-        if( $this->is_live() && $result['content_updated'] ) {
-          wp_update_post( array( 'ID' => $post->ID, 'post_content' => $result['new_content'] ) );
-        }
-      }
+      $convert_error = $result['convert_error'];
+      $conversions_output = $result['conversions_output'];
 
       $percent_done = $total > 0 ? round ( (($offset + $limit) / $total) * 100 ) : 0;
       $left = $total - ($offset + $limit);
 
       // build html output
-      foreach( $conversions_output as $output_data ) {
-        $html[] = "<tr><td><a href='" . get_edit_post_link( $output_data['ID'] ) . "' target='_blank'> #". $output_data['ID'] . "</a></td><td><a href='" . get_permalink( $output_data['ID'] ) ."' target='_blank'>". $output_data['title'] . "</a></td><td>" . $output_data['type'] . "</td><td>". $output_data['shortcode'] . "</td><td>" . $output_data['output'] . "</td><td>" . $output_data['error'] . "</td></tr>";
-      }
-      
-      if( empty($html) && $percent_done == 0 ) {
-        $html[] = "<tr><td colspan='6'>No matching players found.</td></tr>";
-      }
+      $html = $this->build_output_html( $conversions_output, $percent_done );
 
       // response
       echo json_encode(
@@ -100,47 +79,6 @@ abstract class FV_Player_Conversion_Base {
           'convert_error' => $convert_error
         )
       );
-    }
-
-    die();
-  }
-
-  function csv_export() {
-    if( !current_user_can('install_plugins') ) return;
-
-    global $wpdb;
-
-    $filename = $this->slug . '-export-' . date('Y-m-d') . '.csv';
-
-    header('Content-type: text/csv');
-    header("Content-Disposition: attachment; filename=$filename");
-    header("Pragma: no-cache");
-    header("Expires: 0");
-
-    $meta_key = '_fv_player_' . $this->slug . '_failed';
-
-    $sql = $wpdb->prepare( "SELECT {$wpdb->postmeta}.meta_value FROM {$wpdb->postmeta} JOIN {$wpdb->posts} ON {$wpdb->postmeta}.post_id = {$wpdb->posts}.ID WHERE {$wpdb->postmeta}.meta_key = '%s' ORDER BY {$wpdb->posts}.post_date_gmt DESC ", $meta_key );
-
-    $results = $wpdb->get_col( $sql );
-
-    if( !empty( $results ) ) {
-      $fp = fopen('php://output', 'wb');
-
-      $header = array('ID','Title','Post-Link','Edit-Link','Shortcode','Message');
-
-      fputcsv($fp, $header);
-
-      foreach( $results as $result ) {
-        $unserialized = unserialize( $result );
-  
-        foreach( $unserialized as $row ) {
-          $row['post_link'] = htmlspecialchars_decode( $row['post_link'] );
-          $row['post_edit'] = htmlspecialchars_decode( $row['post_edit'] );
-          fputcsv($fp, $row);
-        }
-      }
-
-      fclose($fp);
     }
 
     die();
@@ -172,7 +110,7 @@ abstract class FV_Player_Conversion_Base {
         <?php echo wpautop($this->help); ?>
         <p>
           <input type="hidden" name="action" value="rebuild" />
-          
+
           <?php // This checkbox shows the JS confirmation box when clicked to enable ?>
           <input type="checkbox" name="make-changes" id="make-changes" value="1" onclick="if( this.checked ) return confirm('<?php _e('Please make sure you backup your database before continuing. You can use post revisions to get back to previous version of your posts as well.', 'fv-wordpress-flowplayer') ?>') " /> <label for="make-changes">Make changes</label>
 
@@ -190,12 +128,9 @@ abstract class FV_Player_Conversion_Base {
         <table class="wp-list-table widefat fixed striped table-view-list posts">
           <thead>
             <tr>
-              <th style="width: 5em">ID</th>
-              <th>Title</th>
-              <th style="width: 5em">Post Type</th>
-              <th>Shortcode</th>
-              <th>Result</th>
-              <th>Error</th>
+              <?php foreach( $this->screen_fields as $field ) : ?>
+                <th><?php echo $field; ?></th>
+              <?php endforeach; ?>
             </tr>
           </thead>
           <tbody id="output"></tbody>
@@ -216,7 +151,7 @@ abstract class FV_Player_Conversion_Base {
       </script>
     <?php
   }
-  
+
   function is_live() {
     return (!empty($_POST['make-changes']) && $_POST['make-changes'] == 'true') || $this->set_live ;
   }
