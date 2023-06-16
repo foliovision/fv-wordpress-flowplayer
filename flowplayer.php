da9ecff3944dbd732adef9be744fbf339c3fbf39
Michal <michal.kacir@gmail.com>
2023-06-16 12:23:25 +0200

    flowplayer: check DOING_CRON

--- flowplayer.php
+++ flowplayer.php
@@ -88,8 +88,8 @@ include_once(dirname( __FILE__ ) . '/models/xml-video-sitemap.php');
 global $fv_fp;
 $fv_fp = new flowplayer_frontend();
 
-// If it's in wp-admin or if it's Gutenberg post saving
-if( is_admin() || $_SERVER['REQUEST_METHOD'] == "POST" && preg_match( '~/wp-json/wp/v2/posts/\d+~', $_SERVER['REQUEST_URI'] ) ) {
+// If it's in wp-admin, cron or if it's Gutenberg post saving
+if( defined( 'DOING_CRON' ) || (is_admin() || $_SERVER['REQUEST_METHOD'] == "POST" && preg_match( '~/wp-json/wp/v2/posts/\d+~', $_SERVER['REQUEST_URI'] ) ) ) {
   include_once( dirname( __FILE__ ) . '/controller/backend.php' );
   include_once( dirname( __FILE__ ) . '/controller/editor.php' );
   include_once( dirname( __FILE__ ) . '/controller/settings.php' );
