9ed3d8a44d6dde96c01b1acfeea6534966ebbcdd
fvmartin <fvmartin@users.noreply.github.com>
2023-07-12 15:41:14 +0200

    Gulp

--- flowplayer.php
+++ flowplayer.php
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $fv_wp_flowplayer_ver;
-$fv_wp_flowplayer_ver = '7.9.3.25';
+$fv_wp_flowplayer_ver = '7.9.4';
 $fv_wp_flowplayer_core_ver = '7.2.14.12';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
