4caaf32de12a9190978b12e71c269f49e4ac00c8
Michal <michal.kacir@gmail.com>
2023-08-01 14:07:01 +0200

    block editor: compilation

---
+++ blocks/index.js
@@ -0,0 +1,103 @@
+import { __ } from '@wordpress/i18n';
+import { createElement } from '@wordpress/element';
+import { registerBlockType } from '@wordpress/blocks';
+import { InspectorControls, MediaUpload } from '@wordpress/block-editor';
+import { SVG, Path, PanelBody, TextControl, TextareaControl, Button } from '@wordpress/components';
+
+registerBlockType( 'fv-player-gutenberg/basic', {
+  icon: {
+    foreground: '#C20B33',
+    src: createElement(
+      SVG, {
+        viewBox: "0 0 24 24"
+      }, createElement(Path, {
+        d: "M21.8 8s-.195-1.377-.795-1.984c-.76-.797-1.613-.8-2.004-.847-2.798-.203-6.996-.203-6.996-.203h-.01s-4.197 0-6.996.202c-.39.046-1.242.05-2.003.846C2.395 6.623 2.2 8 2.2 8S2 9.62 2 11.24v1.517c0 1.618.2 3.237.2 3.237s.195 1.378.795 1.985c.76.797 1.76.77 2.205.855 1.6.153 6.8.2 6.8.2s4.203-.005 7-.208c.392-.047 1.244-.05 2.005-.847.6-.607.795-1.985.795-1.985s.2-1.618.2-3.237v-1.517C22 9.62 21.8 8 21.8 8zM9.935 14.595v-5.62l5.403 2.82-5.403 2.8z"
+      })
+    )
+  },
+  title: __( 'FV Player', 'fv-player-gutenberg' ),
+  description: __( 'Embed a video from your Media Library or upload a new one.', 'fv-player-gutenberg' ),
+  category: 'media',
+  keywords: ['fv player', 'player', 'fv', 'flowplayer'],
+  attributes: {
+    src: {
+      type: 'string',
+      default: ''
+    },
+    splash: {
+      type: 'string',
+      default: ''
+    },
+    title: {
+      type: 'string',
+      default: ''
+    },
+    shortcodeContent: {
+      type: 'string',
+      default: '',
+    },
+  },
+  edit: ({ attributes, setAttributes, clientId }) => {
+    const { src, splash, title } = attributes;
+
+    return (
+      <>
+        <InspectorControls>
+          <PanelBody title="Player Settings" initialOpen={true}>
+            <TextControl
+              label="Source URL"
+              value={src}
+              onChange={(value) => setAttributes({ src: value })}
+            />
+            <MediaUpload
+              onSelect={(src) => setAttributes({ src: src.url })}
+              type="video"
+              render={({ open }) => (
+                <Button onClick={open}>Select Video</Button>
+              )}
+            />
+            <TextControl
+              label="Splash URL"
+              value={splash}
+              onChange={(value) => setAttributes({ splash: value })}
+            />
+            <TextControl
+              label="Title"
+              value={title}
+              onChange={(value) => setAttributes({ title: value })}
+            />
+          </PanelBody>
+        </InspectorControls>
+
+        <div className="fv-player-gutenberg">
+          <h4>{title}</h4>
+          <Button className="fv-wordpress-flowplayer-button">Open in Editor</Button>
+        </div>
+
+        <TextareaControl
+            // value={shortcode}
+            // onChange={(value) => setAttributes({ shortcode: value })}
+            className="fv-player-editor-field" // Add custom class to the textarea
+          />
+      </>
+    );
+  },
+  save: ({ attributes }) => {
+    const { src, splash, title } = attributes;
+
+    return (
+      <>
+        <div className="fv-player-gutenberg">
+          <h3>{title}</h3>
+          <Button className="fv-wordpress-flowplayer-button">Open in Editor</Button>
+        </div>
+
+        <TextareaControl
+            // value={shortcode}
+            // onChange={(value) => setAttributes({ shortcode: value })}
+            className="fv-player-editor-field" // Add custom class to the textarea
+        />
+      </>
+    );
+  }
+});
--- models/gutenberg.php
+++ models/gutenberg.php
@@ -5,13 +5,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function fv_player_gutenberg() {
-  global $fv_wp_flowplayer_ver;
-  wp_register_script( 'fv-player-gutenberg', flowplayer::get_plugin_url().'/js/gutenberg.js', array( 'wp-blocks', 'wp-element', 'wp-components' ), filemtime( __DIR__.'/../js/gutenberg.js' ) );
-  
+  wp_register_script( 'fv-player-gutenberg', flowplayer::get_plugin_url().'/build/index.js', array( 'wp-blocks', 'wp-element', 'wp-components'), filemtime( __DIR__.'/../build/index.js' ) );
+
   if( function_exists('register_block_type') ) {
     register_block_type( 'fv-player-gutenberg/basic', array(
       'editor_script' => 'fv-player-gutenberg',
     ) );
   }
 }
+
 add_action( 'init', 'fv_player_gutenberg' );
