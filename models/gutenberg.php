66c4cbca84303a6d198d2cb7b22e0f85f03e3def
Michal <michal.kacir@gmail.com>
2023-08-02 13:53:38 +0200

    block: dependencies update

--- models/gutenberg.php
+++ models/gutenberg.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function fv_player_gutenberg() {
-  wp_register_script( 'fv-player-gutenberg', flowplayer::get_plugin_url().'/build/index.js', array( 'wp-blocks', 'wp-element', 'wp-components'), filemtime( __DIR__.'/../build/index.js' ) );
+  wp_register_script( 'fv-player-gutenberg', flowplayer::get_plugin_url().'/build/index.js', array( 'wp-blocks', 'wp-element','wp-editor', 'wp-components', 'wp-i18n'), filemtime( __DIR__.'/../build/index.js' ) );
 
   wp_localize_script( 'fv-player-gutenberg', 'fv_player_gutenberg', array(
     'nonce' => wp_create_nonce( 'fv_player_gutenberg' ),
