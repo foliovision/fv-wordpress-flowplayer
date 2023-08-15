f8f48c0b29c34236ca512f29a352bfe2af945204
fvmartin <fvmartin@users.noreply.github.com>
2023-08-15 10:43:39 +0200

    Freedom Player support for Ajax video URL loading

--- flowplayer.php
+++ flowplayer.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '7.9.8';
-$fv_wp_flowplayer_core_ver = '7.2.14.12';
+$fv_wp_flowplayer_core_ver = '7.2.14.13';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
   include_once( dirname( __FILE__ ) . '/includes/module.php' );
--- flowplayer/modules/fv-player.js
+++ flowplayer/modules/fv-player.js
@@ -176,7 +176,7 @@ jQuery(document).ready( function() {
     if( loading_count < 1000 && (
       window.fv_video_intelligence_conf && !window.FV_Player_IMA ||
       window.fv_vast_conf && !window.FV_Player_IMA ||
-      window.fv_player_pro && !window.FV_Flowplayer_Pro && document.getElementById('fv_player_pro') != fv_player_pro ||
+      window.fv_player_pro && !window.FV_Player_Pro && document.getElementById('fv_player_pro') != fv_player_pro ||
       window.fv_player_user_playlists && !window.fv_player_user_playlists.is_loaded ||
       // if using FV Player JS Loader wait until all scripts have finished loading
       window.FV_Player_JS_Loader_scripts_total && window.FV_Player_JS_Loader_scripts_loaded < window.FV_Player_JS_Loader_scripts_total
