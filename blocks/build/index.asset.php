e4c81322f0c327ad7b87dfee7b016158f55b8176
Michal <michal.kacir@gmail.com>
2023-08-25 13:52:26 +0200

    block: build

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'd13105c4a8b0c6bdecdc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'fd1deaf8c501f3ea721a');
