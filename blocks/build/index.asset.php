8234dc349bc2063f83ed530304f0dd47cf775537
Michal <michalk12456@gmail.com>
2023-08-31 10:17:29 +0200

    block: initial state structure update

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '6be6d59d6dbb2f24b446');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4c87ce3f7ae5b03272b7');
