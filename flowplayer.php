8935ecbc512fe705e4eb87d98a36170e90dd2edf
fvmartin <fvmartin@users.noreply.github.com>
2023-09-13 14:49:14 +0200

    Gulp js

--- flowplayer.php
+++ flowplayer.php
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $fv_wp_flowplayer_ver;
 
-$fv_wp_flowplayer_ver = '7.9.14';
+$fv_wp_flowplayer_ver = '7.9.15';
 $fv_wp_flowplayer_core_ver = '7.2.14.14';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
