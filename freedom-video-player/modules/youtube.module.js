50ea8372cd3db6b81172c58dab0fce0f6ff854f3
Michal <michal.kacir@gmail.com>
2023-09-20 14:29:53 +0200

    youtube: youtube.moduel.js

--- freedom-video-player/modules/youtube.module.js
+++ freedom-video-player/modules/youtube.module.js
@@ -19,6 +19,8 @@ flowplayer(function (api, root) {
     if( video.type == 'video/youtube' ) {
       root.addClass('is-youtube');
 
+      debugger;
+
       if( typeof fv_flowplayer_conf.youtube_browser_chrome != 'undefined' ) {
         var youtube_icon = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24"\ stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">\
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>\
