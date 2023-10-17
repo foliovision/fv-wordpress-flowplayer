62d47037bc743153c042d842387269833d98a3d3
fvmartin <fvmartin@users.noreply.github.com>
2023-10-17 11:37:25 +0200

    Exclude vendor/ from repository
    Including a warning if somebody uses the plugin without that folder and tries to browser S3 or compatible storage to pick videos.

--- includes/aws/aws-autoloader.php
+++ includes/aws/aws-autoloader.php
@@ -1,3 +1,5 @@
 <?php
 // Legacy, load the current vendor autload
-require_once( dirname(__FILE__) . "/../../vendor/autoload.php" );
+if ( file_exists( dirname(__FILE__) . "/../../vendor/autoload.php" ) ) {
+  require_once( dirname(__FILE__) . "/../../vendor/autoload.php" );
+}
