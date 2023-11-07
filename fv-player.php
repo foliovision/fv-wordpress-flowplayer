1dc9470efff2f59b2e8208d40824bf4c86c59091
fvmartin <fvmartin@users.noreply.github.com>
2023-11-07 16:49:28 +0100

    Freedom Video Player update

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.beta.14';
-$fv_wp_flowplayer_core_ver = '7.2.16';
+$fv_wp_flowplayer_core_ver = '7.2.17';
 include_once( dirname( __FILE__ ) . '/includes/extra-functions.php' );
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
   include_once( dirname( __FILE__ ) . '/includes/module.php' );
