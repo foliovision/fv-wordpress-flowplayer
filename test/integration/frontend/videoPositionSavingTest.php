fd2eba973c04f13e42e616be535ac80d215dee6a
fvmartin <fvmartin@users.noreply.github.com>
2023-11-09 11:47:08 +0100

    Tests: Position saving
    Count playlist items and check properties instead of full HTML check

--- test/integration/frontend/videoPositionSavingTest.php
+++ test/integration/frontend/videoPositionSavingTest.php
@@ -61,7 +61,7 @@ final class FV_Player_videoPositionSavingTestCase extends FV_Player_Ajax_UnitTes
     } catch ( WPAjaxDieContinueException $e ) {
       $response = json_decode( $this->_last_response );
       $this->assertIsObject($response );
-      $this->assertObjectHasAttribute( 'success', $response );
+      $this->assertObjectHasProperty( 'success', $response );
       $this->assertFalse( $response->success );
     }
 
@@ -69,23 +69,9 @@ final class FV_Player_videoPositionSavingTestCase extends FV_Player_Ajax_UnitTes
     $post = get_post( $this->postID );
     $output = apply_filters( 'the_content', $post->post_content );
 
-    $sample = <<< HTML
-<div id="wpfp_31180ef298e0fc79eff36d1114e09913" class="freedomplayer flowplayer no-brand is-splash is-paused skin-slim no-svg fp-slim fp-edgy has-playlist has-playlist-horizontal" data-fv-embed="?fv_player_embed=1" style="max-width: 100%; " data-ratio="0.5625" data-save-position="yes">
-	<div class="fp-ratio" style="padding-top: 56.25%"></div>
-	<div class="fp-ui"><noscript>Please enable JavaScript</noscript><div class="fp-play fp-visible"><svg class="fp-play-sharp-fill" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.fp-color-play{opacity:0.65;}.controlbutton{fill:#fff;}</style></defs><title>play-sharp-fill</title><path class="fp-color-play" d="M49.9217-.078a50,50,0,1,0,50,50A50.0564,50.0564,0,0,0,49.9217-.078Z"/><polygon class="controlbutton" points="73.601 50 37.968 70.573 37.968 29.427 73.601 50" filter="url(#f1)"/></svg></div><div class="fp-preload"><b></b><b></b><b></b><b></b></div></div>
-<div class='fvp-share-bar'><ul class="fvp-sharing">
-    <li><a class="sharing-facebook" href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank"></a></li>
-    <li><a class="sharing-twitter" href="https://twitter.com/intent/tweet?text=Test+Blog+&url=" target="_blank"></a></li>
-    <li><a class="sharing-email" href="mailto:?body=Check%20out%20the%20amazing%20video%20here%3A%20" target="_blank"></a></li></ul><div><label><a class="embed-code-toggle" href="#"><strong>Embed</strong></a></label></div><div class="embed-code"><label>Copy and paste this HTML code into your webpage to embed.</label><textarea></textarea></div></div>
-</div>
-	<div class="fp-playlist-external fv-playlist-design-2017 fp-playlist-horizontal skin-slim" rel="wpfp_31180ef298e0fc79eff36d1114e09913" id="wpfp_31180ef298e0fc79eff36d1114e09913_playlist">
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/1.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/2.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/3.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-	</div>
-HTML;
-
-    $this->assertEquals( $this->fix_newlines($sample), $this->fix_newlines($output) );
+    $this->assertTrue( substr_count( $output, "data-item" ) == 3 );
+    $this->assertTrue( substr_count( $output, '"position"' ) == 0 );
+    $this->assertTrue( substr_count( $output, '"saw":true' ) == 0 );
   }
 
   // For logged in users the video position saving Ajax should affect the FV Player output - adding position to data-item
@@ -117,7 +103,7 @@ HTML;
     } catch ( WPAjaxDieContinueException $e ) {
       $response = json_decode( $this->_last_response );
       $this->assertIsObject( $response );
-      $this->assertObjectHasAttribute( 'success', $response );
+      $this->assertObjectHasProperty( 'success', $response );
       $this->assertTrue( $response->success );
     }
 
@@ -129,23 +115,9 @@ HTML;
     $post = get_post( $this->postID );
     $output = apply_filters( 'the_content', $post->post_content );
 
-    $sample = <<< HTML
-<div id="wpfp_245a181e8fd0e4cbe48d6e34cd579eda" class="freedomplayer flowplayer no-brand is-splash is-paused skin-slim no-svg fp-slim fp-edgy has-playlist has-playlist-horizontal" data-fv-embed="?fv_player_embed=1" style="max-width: 100%; " data-ratio="0.5625" data-save-position="yes">
-	<div class="fp-ratio" style="padding-top: 56.25%"></div>
-	<div class="fp-ui"><noscript>Please enable JavaScript</noscript><div class="fp-play fp-visible"><svg class="fp-play-sharp-fill" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.fp-color-play{opacity:0.65;}.controlbutton{fill:#fff;}</style></defs><title>play-sharp-fill</title><path class="fp-color-play" d="M49.9217-.078a50,50,0,1,0,50,50A50.0564,50.0564,0,0,0,49.9217-.078Z"/><polygon class="controlbutton" points="73.601 50 37.968 70.573 37.968 29.427 73.601 50" filter="url(#f1)"/></svg></div><div class="fp-preload"><b></b><b></b><b></b><b></b></div></div>
-<div class='fvp-share-bar'><ul class="fvp-sharing">
-    <li><a class="sharing-facebook" href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank"></a></li>
-    <li><a class="sharing-twitter" href="https://twitter.com/intent/tweet?text=Test+Blog+&url=" target="_blank"></a></li>
-    <li><a class="sharing-email" href="mailto:?body=Check%20out%20the%20amazing%20video%20here%3A%20" target="_blank"></a></li></ul><div><label><a class="embed-code-toggle" href="#"><strong>Embed</strong></a></label></div><div class="embed-code"><label>Copy and paste this HTML code into your webpage to embed.</label><textarea></textarea></div></div>
-</div>
-	<div class="fp-playlist-external fv-playlist-design-2017 fp-playlist-horizontal skin-slim" rel="wpfp_245a181e8fd0e4cbe48d6e34cd579eda" id="wpfp_245a181e8fd0e4cbe48d6e34cd579eda_playlist">
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/1.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/2.mp4","type":"video\/mp4","position":12,"top_position":32}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/3.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img no-image'></div></a>
-	</div>
-HTML;
-
-    $this->assertEquals( $this->fix_newlines($sample), $this->fix_newlines($output) );
+    $this->assertTrue( substr_count( $output, "data-item" ) == 3 );
+    $this->assertTrue( substr_count( $output, '"position":12,"top_position":32' ) == 1 );
+    $this->assertTrue( substr_count( $output, '"saw":true' ) == 0 );
 
     // another request, this time with lower top position being sent
     $this->_last_response = '';
@@ -164,7 +136,7 @@ HTML;
     } catch ( WPAjaxDieContinueException $e ) {
       $response = json_decode( $this->_last_response );
       $this->assertIsObject( $response );
-      $this->assertObjectHasAttribute( 'success', $response );
+      $this->assertObjectHasProperty( 'success', $response );
       $this->assertTrue( $response->success );
     }
 
@@ -191,7 +163,7 @@ HTML;
     } catch ( WPAjaxDieContinueException $e ) {
       $response = json_decode( $this->_last_response );
       $this->assertIsObject( $response );
-      $this->assertObjectHasAttribute( 'success', $response );
+      $this->assertObjectHasProperty( 'success', $response );
       $this->assertTrue( $response->success );
     }
 
@@ -206,21 +178,8 @@ HTML;
     $post = get_post( $this->postID );
     $output = apply_filters( 'the_content', $post->post_content );
 
-    $sample = <<< HTML
-<div id="wpfp_55754a50dd0a87c73e1de6be4f08b9a1" class="flowplayer no-brand is-splash is-paused skin-slim no-svg fp-slim fp-edgy has-playlist has-playlist-horizontal" data-fv-embed="?fv_player_embed=2" style="max-width: 100%; " data-ratio="0.5625" data-save-position="yes">
-	<div class="fp-ratio" style="padding-top: 56.25%"></div>
-	<div class="fp-ui"><noscript>Please enable JavaScript</noscript><div class="fp-play fp-visible"><svg class="fp-play-sharp-fill" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><defs><style>.fp-color-play{opacity:0.65;}.controlbutton{fill:#fff;}</style></defs><title>play-sharp-fill</title><path class="fp-color-play" d="M49.9217-.078a50,50,0,1,0,50,50A50.0564,50.0564,0,0,0,49.9217-.078Z"/><polygon class="controlbutton" points="73.601 50 37.968 70.573 37.968 29.427 73.601 50" filter="url(#f1)"/></svg></div><div class="fp-preload"><b></b><b></b><b></b><b></b></div></div>
-<div class='fvp-share-bar'><ul class="fvp-sharing">
-    <li><a class="sharing-facebook" href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank"></a></li>
-    <li><a class="sharing-twitter" href="https://twitter.com/intent/tweet?text=Test+Blog+&url=" target="_blank"></a></li>
-    <li><a class="sharing-email" href="mailto:?body=Check%20out%20the%20amazing%20video%20here%3A%20" target="_blank"></a></li></ul><div><label><a class="embed-code-toggle" href="#"><strong>Embed</strong></a></label></div><div class="embed-code"><label>Copy and paste this HTML code into your webpage to embed.</label><textarea></textarea></div></div>
-</div>
-	<div class="fp-playlist-external fv-playlist-design-2017 fp-playlist-horizontal skin-slim" rel="wpfp_55754a50dd0a87c73e1de6be4f08b9a1">
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/1.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img'><div class='fvp-playlist-thumb-img no-image'></div></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/2.mp4","type":"video\/mp4","position":12,"saw":true}]}'><div class='fvp-playlist-thumb-img'><div class='fvp-playlist-thumb-img no-image'></div><span class="fvp-progress-wrap"><span class="fvp-progress" style="width: 100%"></span></span></div></a>
-		<a href='#' data-item='{"sources":[{"src":"https:\/\/cdn.site.com\/3.mp4","type":"video\/mp4"}]}'><div class='fvp-playlist-thumb-img'><div class='fvp-playlist-thumb-img no-image'></div></div></a>
-	</div>
-HTML;
+    $this->assertTrue( substr_count( $output, "data-item" ) == 3 );
+    $this->assertTrue( substr_count( $output, '"position":12,"saw":true' ) == 1 );
     
     $current_user;
   }
