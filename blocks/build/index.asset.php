34813739a45f4b28f60a7f769337fe70de8f56da
fvmartin <fvmartin@users.noreply.github.com>
2023-11-27 13:13:54 +0100

    Compile block

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'a93247e29f67ea447b8a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '71801bbaf6c8e83feafd');
