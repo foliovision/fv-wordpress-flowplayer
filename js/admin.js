f2af7baf6d42f30b34209dc756e584e861399fa5
fvmartin <fvmartin@users.noreply.github.com>
2023-12-01 12:16:02 +0100

    New color picker: Fixes for Ajax saving: Fix

--- js/admin.js
+++ js/admin.js
@@ -148,10 +148,12 @@
           style += sanitizeCSS(newStyle);
           
         } else {
-          var value = $this.val().replace(/^#/,'');
-          if( opacity = $this.minicolors('opacity') ) {
-            value = hexToRgb(value);
-            value = 'rgba('+value[0]+','+value[1]+','+value[2]+','+opacity+')';
+          var value = $this.val().replace(/^#/,''),
+            opacity = $this.minicolors('opacity'),
+            color = hexToRgb(value);
+
+          if( opacity && color ) {
+            value = 'rgba('+color[0]+','+color[1]+','+color[2]+','+opacity+')';
           }
           newStyle = preview.replace(/%val%/g, value);
           style += sanitizeCSS(newStyle);
