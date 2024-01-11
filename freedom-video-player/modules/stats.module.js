cdcc7edc0ce3fa00bc9b77292af1ce0fae8006fd
fvmartin <fvmartin@users.noreply.github.com>
2024-01-11 12:20:27 +0100

    Stats: Nonce: Missing nonce for click and seconds

--- freedom-video-player/modules/stats.module.js
+++ freedom-video-player/modules/stats.module.js
@@ -94,7 +94,8 @@
         'player_id': data.player_id,
         'post_id' : data.post_id,
         'user_id' : api.conf.fv_stats.user_id,
-        'tag' : 'click'
+        'tag' : 'click',
+        '_wpnonce' : api.conf.fv_stats.nonce,
       } );
     });
 
@@ -123,6 +124,7 @@
       fd.append( 'tag', 'seconds' );
       fd.append( 'blog_id', conf.fv_stats.blog_id );
       fd.append( 'user_id', conf.fv_stats.user_id );
+      fd.append( '_wpnonce', conf.fv_stats.nonce );
       // TODO: Can we use pure JSON?
       fd.append( 'watched', encodeURIComponent(JSON.stringify(watched)) );
 
