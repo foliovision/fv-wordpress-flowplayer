374f7a809c7891c8ed40cf591aae6e1a1bc296c7
fvmartin <fvmartin@users.noreply.github.com>
2024-05-06 15:26:17 +0200

    Gutenberg: Allow all player clicks in editor

--- freedom-video-player/modules/gutenberg.module.js
+++ freedom-video-player/modules/gutenberg.module.js
@@ -11,7 +11,7 @@ flowplayer( function(api,root) {
   var click_to_edit = '<div title="Click to edit" style="width: 40%; height: calc( 100% - 3em ); z-index: 19; position: absolute; top: 0; left: 0; cursor: context-menu" onclick="return false" title="Click to edit"></div><div style="width: 40%; height: calc( 100% - 3em ); z-index: 19; position: absolute; top: 0; right: 0; cursor: context-menu" onclick="return false" title="Click to edit"></div><div style="width: 20%; height: 40%; z-index: 19; position: absolute; top: 0; right: 40%; cursor: context-menu" onclick="return false" title="Click to edit"></div><div style="width: 20%; height: calc( 40% - 3em ); z-index: 19; position: absolute; top: 60%; right: 40%; cursor: context-menu" onclick="return false"></div>';
 
   // check if we are in the editor
-  if( document.body.classList.contains('block-editor-page') ) {
+  if( document.body.classList.contains('block-editor-page') && ! root.closest( '#fv-player-shortcode-editor-preview-target' ).length ) {
     jQuery( click_to_edit ).insertAfter( root.find('.fp-ratio') );
   }
 
