bb126517d42740901889c9247c92af20e717eebb
continuous-integration <continuous-integration@foliovison.com>
2024-05-31 09:43:17 +0000

    Tests: Avoid FV_Player_Db loading twice and thus some filters being executed twice

--- test/integration/fv-player-unittest-case.php
+++ test/integration/fv-player-unittest-case.php
@@ -1,57 +1,57 @@
 <?php
 
 abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
-  
+
   protected $backupGlobals = false;
   protected $restore;
 
   protected function setUp(): void {
     parent::setUp();
-    
+
     global $fv_fp;
     $this->restore = $fv_fp->conf;
-  }  
-  
+  }
+
   public function fix_newlines( $html ) {
     $html = preg_replace( '/[\'"]wpfp_[0-9a-z]+[\'"]/', '"wpfp_some-test-hash"', $html);
     $html = preg_replace( '/[\'"]wpfp_[0-9a-z]+_playlist[\'"]/', '"wpfp_some-test-hash_playlist"', $html);
     $html = preg_replace( '~<input type="hidden" id="([^"]*?)nonce" name="([^"]*?)nonce" value="([^"]*?)" />~', '<input type="hidden" id="$1nonce" name="$2nonce" value="XYZ" />', $html);
-    $html = preg_replace( '~<input type="hidden" id="nonce_([^"]*?)" name="nonce_([^"]*?)" value="([^"]*?)" />~', '<input type="hidden" id="nonce_$1" name="nonce_$2" value="XYZ" />', $html);    
+    $html = preg_replace( '~<input type="hidden" id="nonce_([^"]*?)" name="nonce_([^"]*?)" value="([^"]*?)" />~', '<input type="hidden" id="nonce_$1" name="nonce_$2" value="XYZ" />', $html);
     $html = preg_replace( "~nonce: '([^']*?)'~", "nonce: 'XYZ'", $html);
-    
+
     // testProfileScreen
     $html = preg_replace( '~fv_ytplayer_[a-z0-9]+~', 'fv_ytplayer_XYZ', $html);
     $html = preg_replace( '~fv_vimeo_[a-z0-9]+~', 'fv_vimeo_XYZ', $html);
     $html = preg_replace( '~<input type="hidden" id="fv-player-custom-videos-_fv_player_user_video-0" name="fv-player-custom-videos-_fv_player_user_video-0" value="[^"]*?" />~', '<input type="hidden" id="fv-player-custom-videos-_fv_player_user_video-0" name="fv-player-custom-videos-_fv_player_user_video-0" value="XYZ" />', $html);
     $html = preg_replace( "~fv-player-custom-videos-entity-id\[_fv_player_user_video\]' value='\d+'~", "fv-player-custom-videos-entity-id[_fv_player_user_video]' value='1234'", $html);
-    
+
     $html = preg_replace( '~convert_jwplayer=[a-z0-9]+~', 'convert_jwplayer=XYZ', $html);
     $html = preg_replace( '~_wpnonce=[a-z0-9]+~', '_wpnonce=XYZ', $html);
     $html = preg_replace( '~nonce":"[a-z0-9]+"~', 'nonce":"XYZ"', $html);
-    
+
     // XML Video Sitemap
     $html = preg_replace( '~http://example.org/\?p=\d{1,}~', 'http://example.org/?p=1234', $html);
     $html = preg_replace( '~Post excerpt \d{1,}~', 'Post excerpt -regex-replaced-', $html);
-    
+
     // DB IDs in JSON
     $html = preg_replace( '~"id":\d+~', '"id":1234', $html);
 
     // data-player-id on main player DIV
     $html = preg_replace( '~data-player-id=".*?"~', 'data-player-id="some-id-here"', $html);
-    
+
     $html = explode("\n",$html);
     foreach( $html AS $k => $v ) {
       if( trim($v) == '' ) unset($html[$k]);
     }
     $html = implode( "\n", array_map('trim',$html) );
-    
+
     $html = preg_replace( '~\t~', '', $html );
-    
+
     //  playlist in lightbox test
     $html = preg_replace( '/(href|data-fv-lightbox|data-src)="#wpfp_[^"]+"/', '$1="#wpfp_some-test-hash"', $html);
     $html = preg_replace( '/(id|rel)="wpfp_[^"]+"/', '$1="wpfp_some-test-hash"', $html);
     $html = preg_replace( '~fv_flowplayer_[a-z0-9]+_lightbox_starter~', 'fv_flowplayer_XYZ_lightbox_starter', $html);
-    
+
     //  tabbed playlist test
     $html = preg_replace( '~tabs-\d+~', 'tabs-1', $html);
 
@@ -60,9 +60,9 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
 
     // splash end
     $html = preg_replace( '~wpfp_[a-z0-9]+_custom_background~', 'wpfp_XYZ_custom_background', $html);
-    
+
     $html = preg_replace( '~\?ver=[0-9-wpalphabetaRCmod\.]+~', '?ver=1.2.3', $html);
-    
+
     $html = preg_replace( '~<video:publication_date>(.*?)</video:publication_date>~', '<video:publication_date>2019-04-23T09:44:33+00:00</video:publication_date>', $html);
 
     // plugin_url() giving bad paths as FV Player folder is above the WordPress folder (test/testSuite), more: https://core.trac.wordpress.org/ticket/34358
@@ -71,7 +71,7 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
 
     // Settings page preview link
     $html = preg_replace( '~http://example.org\?fv_player_embed=.*?&fv_player_preview=~', 'http://example.org?fv_player_embed=SOME_NONCE?&fv_player_preview=', $html);
-    
+
     // System Info - gone for now
     $html = preg_replace( '~### Begin System Info ###[\s\S]*?### End System Info ###~', '$1: some value', $html);
 
@@ -79,9 +79,9 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
 
     /**
      * Somehow WordPress puts 2 spaces into the <link> tags for CSS:
-     * 
+     *
      * <link rel='stylesheet' id='fv_flowplayer-css'  href='http://example.org/wp-content/plugins/fv-wordpress-flowplayer/css/flowplayer.css?ver=1.2.3' type='text/css' media='all' />
-     * 
+     *
      * So we avoid that here.
      */
     $html = preg_replace( "~(<link rel='stylesheet'.*)  ~", '$1 ', $html );
@@ -97,7 +97,7 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
     );
 
     $html = preg_replace( '~<script type="text/javascript" id="([^"]+)">~', "<script type='text/javascript' id='$1'>", $html );
-    
+
     // We lazy load splash images except the first one, but it's hard to keep track of that in-between the tests
     $html = str_replace( '<img loading="lazy" ', '<img ', $html );
 
@@ -112,7 +112,7 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
     //include_once "../../../fv-wordpress-flowplayer/controller/backend.php";
 
     // include the flowplayer loader
-    include "../../../fv-wordpress-flowplayer/fv-player.php";
+    include_once "../../../fv-wordpress-flowplayer/fv-player.php";
 
     // include the PRO plugin class, so it can intercept data saving
     // and update the ads structure as needed for saving
@@ -121,12 +121,12 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
     // save initial settings
     //$fv_fp->_set_conf();
   }
-  
+
   protected function tearDown(): void {
     parent::tearDown();
-    
+
     global $fv_fp;
     $fv_fp->conf = $this->restore;
-  }  
+  }
 
 }
