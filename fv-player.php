8b0553dad367f1779d7e7ed1c02f4f00385bc6ac
fvmartin <fvmartin@users.noreply.github.com>
2024-07-11 13:44:25 +0200

    Freedom Player Update - mobile hover controls fix
    They would not disappear on tap when the player was further down on the page once you entered fullscreen
    So you had to wait for them to go away.

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.beta.29';
-$fv_wp_flowplayer_core_ver = '8.0.beta.22.107';
+$fv_wp_flowplayer_core_ver = '8.0.beta.22.108';
 
 if( file_exists( dirname( __FILE__ ) . '/includes/module.php' ) ) {
   include_once( dirname( __FILE__ ) . '/includes/module.php' );
