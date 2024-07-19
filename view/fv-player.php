5456d18b114e5a3decf09ee8e3b28c896ba01b26
fvmartin <fvmartin@users.noreply.github.com>
2024-07-19 14:23:45 +0200

    wp-admin Menu: No longer add Settings -> FV Player: Fix title

--- view/fv-player.php
+++ view/fv-player.php
@@ -3,7 +3,7 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 class FV_Player_List_Table_View {
-  
+
   var $list_page = false;
 
   function __construct() {
@@ -14,7 +14,7 @@ class FV_Player_List_Table_View {
     if( current_user_can('edit_posts')  ) {
       add_menu_page( 'FV Player', 'FV Player', 'edit_posts', 'fv_player', '', flowplayer::get_plugin_url().'/images/icon@x2.png', 30 );
       $this->list_page = add_submenu_page(  'fv_player', 'FV Player', 'Videos/Playlists', 'edit_posts', 'fv_player', array($this, 'tools_panel') );
-      
+
       add_action( 'load-'.$this->list_page, array( $this, 'screen_options' ) );
       add_filter( 'manage_toplevel_page_fv_player_columns', array( $this, 'screen_columns' ) );
       add_filter( 'hidden_columns', array( $this, 'screen_columns_hidden' ), 10, 3 );
@@ -22,9 +22,9 @@ class FV_Player_List_Table_View {
   }
 
   function settings_link() {
-    add_submenu_page(  'fv_player', 'Settings Link', 'Settings', 'manage_options', 'fvplayer', 'fv_player_admin_page' );
+    add_submenu_page(  'fv_player', 'FV Player Settings', 'Settings', 'manage_options', 'fvplayer', 'fv_player_admin_page' );
   }
-  
+
   function load_options() {
     add_action( 'admin_menu', array($this, 'admin_menu') );
 
@@ -35,28 +35,28 @@ class FV_Player_List_Table_View {
     add_filter( 'set-screen-option', array($this, 'set_screen_option'), 10, 3);
     add_filter( 'set_screen_option_fv_player_per_page', array($this, 'set_screen_option'), 10, 3);
   }
-  
+
   function set_screen_option($status, $option, $value) {
     if( 'fv_player_per_page' == $option ) return $value;
     return $status;
   }
-  
+
   function screen_columns() {
     return FV_Player_List_Table::get_columns_worker();
   }
-  
+
   function screen_columns_hidden( $hidden, $screen, $use_defaults ) {
     if( $use_defaults && $screen->id == $this->list_page) {
       $hidden = array( 'subtitles_count', 'chapters_count', 'transcript_count', 'author' );
     }
     return $hidden;
   }
-  
+
   function screen_options() {
     $screen = get_current_screen();
     if(!is_object($screen) || $screen->id != $this->list_page)
       return;
-   
+
     $args = array(
       'label' => __( 'Players per page', 'fv-player' ),
       'default' => 25,
@@ -64,11 +64,11 @@ class FV_Player_List_Table_View {
     );
     add_screen_option( 'per_page', $args );
   }
-  
+
   function styling() {
     global $fv_wp_flowplayer_ver;
     wp_enqueue_style('fv-player-list-view', flowplayer::get_plugin_url().'/css/list-view.css',array(), $fv_wp_flowplayer_ver );
-    
+
     wp_enqueue_media();
   }
 
@@ -77,9 +77,9 @@ class FV_Player_List_Table_View {
     <style>#adminmenu #toplevel_page_fv_player .wp-menu-image img {width:28px;height:25px;padding-top:4px !important}</style>
     <?php
   }
-  
+
   function tools_panel() {
-    
+
     $user = get_current_user_id();
     $screen = get_current_screen();
     $screen_option = $screen->get_option('per_page', 'option');
@@ -90,7 +90,7 @@ class FV_Player_List_Table_View {
     $table = new FV_Player_List_Table( array(
       'per_page' => $per_page
     ) );
-    
+
     $table->prepare_items();
     ?>
     <div class="wrap">
@@ -141,14 +141,14 @@ class FV_Player_List_Table_View {
               <?php $table->display() ?>
           </form>
       </div>
-    
+
     </div>
-  <?php 
+  <?php
     fv_player_shortcode_editor_scripts_enqueue();
     fv_wp_flowplayer_edit_form_after_editor();
 
     wp_enqueue_script( 'fv-player-list-view', flowplayer::get_plugin_url().'/js/list-table.js', array('jquery'), filemtime( dirname(__FILE__).'/../js/list-table.js' ), true );
-  }  
+  }
 }
 
 $FV_Player_List_Table_View = new FV_Player_List_Table_View;
