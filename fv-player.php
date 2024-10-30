312ad9d9dcc1a342d9a247339b83b1fdc0b2471b
Martin FV <martinv@foliovision.com>
2024-10-30 17:48:16 +0100

    Gulp

--- fv-player.php
+++ fv-player.php
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $fv_wp_flowplayer_ver;
 
-$fv_wp_flowplayer_ver = '8.0.7.5';
+$fv_wp_flowplayer_ver = '8.0.7.6';
 $fv_wp_flowplayer_core_ver = '8.0.8.1';
 
 function fv_player_8_activate() {
