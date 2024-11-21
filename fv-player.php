ca6766b72177f56a4eaba7f36154ed5f13a8806b
fvmartin <fvmartin@users.noreply.github.com>
2024-11-21 12:16:30 +0100

    Gulp

--- fv-player.php
+++ fv-player.php
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $fv_wp_flowplayer_ver;
 
-$fv_wp_flowplayer_ver = '8.0.11';
+$fv_wp_flowplayer_ver = '8.0.11.1';
 $fv_wp_flowplayer_core_ver = '8.0.8.4';
 
 function fv_player_8_activate() {
