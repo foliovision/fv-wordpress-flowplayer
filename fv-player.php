645150b0de5c93175dcbf5082536bdeb0d92ae12
fvmartin <fvmartin@users.noreply.github.com>
2024-12-11 15:26:45 +0100

    Freedom Player: Fix Chromecast to use up-to-date Web Sender API

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.13.1';
-$fv_wp_flowplayer_core_ver = '8.0.8.5';
+$fv_wp_flowplayer_core_ver = '8.0.8.6';
 
 function fv_player_8_activate() {
   foreach(
