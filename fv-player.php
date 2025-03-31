ae87c6d07c5dd3639a150b54eb25e9e41dbe5292
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-03-31 14:35:38 +0200

    Gulp

--- fv-player.php
+++ fv-player.php
@@ -35,7 +35,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $fv_wp_flowplayer_ver;
 
-$fv_wp_flowplayer_ver = '8.0.18.2';
+$fv_wp_flowplayer_ver = '8.0.18.3';
 $fv_wp_flowplayer_core_ver = '8.0.18.3';
 
 function fv_player_8_activate() {
