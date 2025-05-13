1c8dcd0a43d5625c91f1d694307107dd1b186057
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-05-13 17:19:22 +0200

    Freedomplayer: Accessibility support: Mobile hotfix

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.19.1';
-$fv_wp_flowplayer_core_ver = '8.0.19';
+$fv_wp_flowplayer_core_ver = '8.0.19.1';
 
 function fv_player_8_activate() {
   foreach(
