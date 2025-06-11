baf96a33ed26370b2328e8171b6ebe289b71d846
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-06-11 12:51:01 +0200

    Freedom Player: Hooks for custom video duration, start and end time This way FV Player Pro does not have to fix the visible time values and timeline behavior

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.19.9';
-$fv_wp_flowplayer_core_ver = '8.0.18.6';
+$fv_wp_flowplayer_core_ver = '8.0.19.9';
 
 function fv_player_8_activate() {
   foreach(
