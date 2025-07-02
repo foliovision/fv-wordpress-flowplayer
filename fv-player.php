9cde76f2749103cf38550054bd85fd35bf097ae3
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-07-02 10:19:04 +0200

    Freedom Player: Video tag crossOrigin property: Use conf.video_cross_origin

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.20';
-$fv_wp_flowplayer_core_ver = '8.0.20';
+$fv_wp_flowplayer_core_ver = '8.0.20.1';
 
 function fv_player_8_activate() {
   foreach(
