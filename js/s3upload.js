bb9365fa450ce985024d172cacbc3bf8e8e272d0
Martin FV <martinv@foliovision.com>
2025-07-08 21:57:05 +0200

    Upload check: Video type check on first 10 MB of file

--- js/s3upload.js
+++ js/s3upload.js
@@ -1,5 +1,6 @@
 function S3MultiUpload(file) {
-    this.PART_SIZE = 100 * 1024 * 1024; // 100 MB per chunk
+    this.FIRST_PART_SIZE = 10 * 1024 * 1024; // 10 MB for first chunk
+    this.PART_SIZE = 100 * 1024 * 1024; // 100 MB per chunk for subsequent chunks
 //    this.PART_SIZE = 5 * 1024 * 1024 * 1024; // Minimum part size defined by aws s3 is 5 MB, maximum 5 GB
     this.SERVER_LOC = window.fv_flowplayer_browser.ajaxurl + '?'; // Location of our server where we'll send all AWS commands and multipart instructions
     this.completed = false;
@@ -67,12 +68,15 @@ S3MultiUpload.prototype.uploadParts = function() {
     var partNum = 0;
 
     while(start < this.file.size) {
-        end = Math.min(start + this.PART_SIZE, this.file.size);
+        // Use first chunk size for the first chunk, regular size for subsequent chunks
+        var currentPartSize = (partNum === 0) ? this.FIRST_PART_SIZE : this.PART_SIZE;
+        end = Math.min(start + currentPartSize, this.file.size);
         var filePart = this.file.slice(start, end);
         // this is to prevent push blob with 0Kb
         if (filePart.size > 0)
             blobs.push(filePart);
-        start = this.PART_SIZE * ++partNum;
+        start += currentPartSize;
+        partNum++;
     }
 
     for (var i = 0; i < blobs.length; i++) {
@@ -140,6 +144,9 @@ S3MultiUpload.prototype.sendToS3 = function(data, blob, index) {
                     }
                     return;
                 }
+                
+                // Chunk completed successfully
+                self.onChunkCompleted(index, blob, data);
             }
             self.updateProgress();
         }
@@ -289,3 +296,81 @@ S3MultiUpload.prototype.onUploadCompleted = function(serverData) {};
  *
  */
 S3MultiUpload.prototype.onPrepareCompleted = function() {};
+
+/**
+ * Override this method to execute something when a chunk completes uploading
+ *
+ * @param {number} index The chunk index (0-based)
+ * @param {Blob} blob The chunk blob data
+ * @param {object} data The response data from the server
+ */
+S3MultiUpload.prototype.onChunkCompleted = function(index, blob, data) {
+    // If this is the first chunk, validate if it's a video
+    if (index === 0) {
+        this.validateVideo(blob).then((isVideo) => {
+            this.onVideoValidationComplete(isVideo, blob);
+        });
+    }
+};
+
+/**
+ * Validates if the file is a video by checking file headers
+ * @param {Blob} blob The file blob to validate
+ * @returns {Promise<boolean>} True if the file is a valid video
+ */
+S3MultiUpload.prototype.validateVideo = function(blob) {
+    return new Promise((resolve) => {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var arr = new Uint8Array(e.target.result);
+            var header = '';
+            
+            // Convert first 12 bytes to hex string
+            for (var i = 0; i < Math.min(12, arr.length); i++) {
+                header += arr[i].toString(16).padStart(2, '0');
+            }
+            
+            // Check for common video file signatures
+            var isVideo = false;
+            
+            // MP4 signature: ftyp
+            if (header.indexOf('66747970') !== -1) {
+                isVideo = true;
+            }
+            // AVI signature: RIFF....AVI
+            else if (header.indexOf('52494646') !== -1 && header.indexOf('41564920') !== -1) {
+                isVideo = true;
+            }
+            // MOV signature: ftyp
+            else if (header.indexOf('66747970') !== -1) {
+                isVideo = true;
+            }
+            // WebM signature: 1a45dfa3
+            else if (header.indexOf('1a45dfa3') !== -1) {
+                isVideo = true;
+            }
+            // FLV signature: 464c5601
+            else if (header.indexOf('464c5601') !== -1) {
+                isVideo = true;
+            }
+            // WMV signature: 3026b275
+            else if (header.indexOf('3026b275') !== -1) {
+                isVideo = true;
+            }
+            // MKV signature: 1a45dfa3
+            else if (header.indexOf('1a45dfa3') !== -1) {
+                isVideo = true;
+            }
+            
+            resolve(isVideo);
+        };
+        reader.readAsArrayBuffer(blob);
+    });
+};
+
+/**
+ * Override this method to handle video validation results
+ * @param {boolean} isVideo True if the file is a valid video
+ * @param {Blob} blob The first chunk blob data
+ */
+S3MultiUpload.prototype.onVideoValidationComplete = function(isVideo, blob) {};
