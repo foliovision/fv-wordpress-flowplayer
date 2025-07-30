c1ac4f1638a55eb3e905f557eb76e8cf40e9f539
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-07-30 16:33:37 +0200

    DigitalOcean Spaces: fix token for URL with spaces: Fix after bad merge

--- models/digitalocean-spaces.class.php
+++ models/digitalocean-spaces.class.php
@@ -182,7 +182,16 @@ class FV_Player_DigitalOcean_Spaces extends FV_Player_CDN {
 
     $time = $ttl ? $ttl : apply_filters('fv_player_secure_link_timeout', 900);
 
-    $url_components = parse_url($url);
+    $url_components = wp_parse_url($url);
+
+    $url_components['path'] = str_replace( array('%20','+'), ' ', $url_components['path']);
+
+    $url_components['path'] = rawurlencode($url_components['path']);
+    $url_components['path'] = str_replace('%2F', '/', $url_components['path']);
+    $url_components['path'] = str_replace('%2B', '+', $url_components['path']);
+    $url_components['path'] = str_replace('%2523', '%23', $url_components['path']);
+    $url_components['path'] = str_replace('%252B', '%2B', $url_components['path']);
+    $url_components['path'] = str_replace('%2527', '%27', $url_components['path']);
 
     // Round the current time to the nearest $time interval to ensure consistent signatures
     // Use ceil() to ensure URLs are valid for the full $time duration
