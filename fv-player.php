531929db615e5fb72d53d49e685489be5e7e89ae
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-09-29 15:52:57 +0200

    Freedom Player: Click to unmute is now part of it: Control bar unmute should not show together with it

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.27.10';
-$fv_wp_flowplayer_core_ver = '8.0.20.3';
+$fv_wp_flowplayer_core_ver = '8.0.20.4';
 
 function fv_player_8_activate() {
   foreach(
