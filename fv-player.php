73ee2fd03f39e20fde290d5b1601a054cc70d7e8
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-10-02 12:37:38 +0200

    Freedom Player: iOS autoplay fix for other videos
    Create <video> for all players an unmute it when user enables sound for a video,
    ensuring they will autoplay with sound

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.0.27.12';
-$fv_wp_flowplayer_core_ver = '8.0.20.4';
+$fv_wp_flowplayer_core_ver = '8.0.27';
 
 function fv_player_8_activate() {
   foreach(
