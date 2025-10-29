2b54537f2c2bf5c6a36d95c823e8a5f7c1503e25
Martin Vicenik <fvmartin@users.noreply.github.com>
2025-10-29 16:01:14 +0100

    Open Graph: Add og:video:iframe

--- models/open-graph.php
+++ models/open-graph.php
@@ -38,6 +38,7 @@ class FV_Player_Open_Graph {
 		}
 
 		// Find video video that is a MP4 without signed URL requirement.
+		$player_for_open_graph = false;
 		$video_for_open_graph = false;
 		$splash               = false;
 
@@ -73,6 +74,7 @@ class FV_Player_Open_Graph {
 							stripos( $video->getSrc(), '.mp4' ) !== false &&
 							apply_filters( 'fv_flowplayer_video_src', $video->getSrc(), array( 'dynamic' => true ) ) === $video->getSrc()
 						) {
+							$player_for_open_graph = $player;
 							$video_for_open_graph = $video;
 						}
 					}
@@ -146,6 +148,9 @@ class FV_Player_Open_Graph {
 			if ( $video_duration ) {
 				$this->tags[] = '<meta property="og:video:duration" content="' . esc_attr( $video_duration ) . '">';
 			}
+
+			$player_url = user_trailingslashit( trailingslashit( get_permalink( $post->ID ) ) . 'fvp-' . $player_for_open_graph->getId() );
+			$this->tags[] = '<meta property="og:video:iframe" content="' . esc_attr( $player_url ) . '">';
 		}
 	}
 
