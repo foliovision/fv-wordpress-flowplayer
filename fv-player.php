a578ef78f78d00cd5611638750fbd6fa592d3ec1
Martin Vicenik <fvmartin@users.noreply.github.com>
2026-08-18 17:20:49 +0200

    Freedom Player: Vertical video fixes

--- fv-player.php
+++ fv-player.php
@@ -36,7 +36,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $fv_wp_flowplayer_ver;
 
 $fv_wp_flowplayer_ver = '8.1.6';
-$fv_wp_flowplayer_core_ver = '8.1.5';
+$fv_wp_flowplayer_core_ver = '8.1.5.1';
 
 function fv_player_8_activate() {
   foreach(
