409e2108d98885212f82ecf6086947ce1b8cbdb6
Martin Vicenik <fvmartin@users.noreply.github.com>
2026-09-15 12:04:42 +0200

    Video Custom Fields: Support saving for WooCommerce Product varations

--- test/integration/fv-player-unittest-case.php
+++ test/integration/fv-player-unittest-case.php
@@ -27,7 +27,7 @@ abstract class FV_Player_UnitTestCase extends WP_UnitTestCase {
     $html = preg_replace( '~fv_ytplayer_[a-z0-9]+~', 'fv_ytplayer_XYZ', $html);
     $html = preg_replace( '~fv_vimeo_[a-z0-9]+~', 'fv_vimeo_XYZ', $html);
     $html = preg_replace( '~<input type="hidden" id="fv-player-custom-videos-_fv_player_user_video-0" name="fv-player-custom-videos-_fv_player_user_video-0" value="[^"]*?" />~', '<input type="hidden" id="fv-player-custom-videos-_fv_player_user_video-0" name="fv-player-custom-videos-_fv_player_user_video-0" value="XYZ" />', $html);
-    $html = preg_replace( "~fv-player-custom-videos-entity-id\[_fv_player_user_video\]' value='\d+'~", "fv-player-custom-videos-entity-id[_fv_player_user_video]' value='1234'", $html);
+    $html = preg_replace( "~fv-player-custom-videos-entity-id\[_fv_player_user_video\]\[\d+\]' value='\d+'~", "fv-player-custom-videos-entity-id[_fv_player_user_video][1234]' value='1234'", $html);
 
     $html = preg_replace( '~convert_jwplayer=[a-z0-9]+~', 'convert_jwplayer=XYZ', $html);
     $html = preg_replace( '~_wpnonce=[a-z0-9]+~', '_wpnonce=XYZ', $html);
